0831d90ccbc34c965ae91152151dfedaf4731835
Sune Simonsen <sune@we-knowhow.dk>
2013-04-08 22:14:29 +0200

    Basic assertions

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -2,4 +2,70 @@
 var expect = require(__dirname + '/../lib/unexpected.js');
 
 describe('unexpected', function () {
+    describe('ok assertion', function () {
+        it('assert that the value is truthy or not', function () {
+            expect(1, 'to be ok');
+            expect(true, 'to be ok');
+            expect(true, 'to not be falsy');
+            expect({}, 'to be truthy');
+            expect(0, 'to not be ok');
+            expect(0, 'to be falsy');
+        });
+    });
+
+    describe('be assertion', function () {
+        it('uses === for comparison', function () {
+            var obj = {};
+            expect(obj, 'to be', obj);
+            expect(obj, 'to not be', {});
+            expect(1, 'to be', 1);
+            expect(NaN, 'not to equal', NaN);
+            expect(1, 'not to be', true);
+            expect('1', 'to not be', 1);
+        });
+
+        it('throws when the assertion fails', function () {
+            expect(function () {
+                expect('foo', 'to be', 'bar');
+            }, 'to throw exception', /expected 'foo' to be 'bar'/);
+        });
+    });
+
+    describe('exception assertion', function () {
+        it('fails if no exception is thrown', function () {
+            expect(function () {
+                throw new Error();
+            }, 'to throw exception');
+        });
+
+        it('given a function the function is called with the exception', function () {
+            expect(function () {
+                throw new SyntaxError();
+            }, 'to throw exception', function (e) {
+                expect(e, 'to be a', SyntaxError);
+            });
+        });
+
+        it('matches the message given regular expression', function () {
+            expect(function () {
+                throw new Error('matches the exception message');
+            }, 'to throw exception', /matches the exception message/);
+            expect(function () {
+                throw new Error('Other error');
+            }, 'to not throw exception', /matches the exception message/);
+        });
+    });
+
+    describe('match assertion', function () {
+        it('tests that the subject matches the given regular expression', function () {
+            expect('test', 'to match', /.*st/);
+            expect('test', 'to not match', /foo/);
+        });
+
+        it('throws when the assertion fails', function () {
+            expect(function () {
+                expect('test', 'to match', /foo/);
+            }, 'to throw exception', /expected 'test' to match \/foo\//);
+        });
+    });
 });
