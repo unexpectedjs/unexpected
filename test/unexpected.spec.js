04e130633142a6edd9e971fc90b880897cb05559
Sune Simonsen <sune@we-knowhow.dk>
2014-02-01 15:30:46 +0100

    Fixing jshint errors

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -1,4 +1,4 @@
-/*global describe, it, expect, beforeEach, setTimeout*/
+/*global describe, it, expect, beforeEach*/
 
 // use this instead of Object.create in order to make the tests run in
 // browsers that are not es5 compatible.
