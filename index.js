78862a08b3d38def09d75933a9c64a8e0f51f894
Andreas Lind Petersen <andreas@one.com>
2014-03-06 22:21:37 +0100

    Don't require a build step in node.js.
    Instead, add a main module that creates a new vm context, then evaluates the sources files in it one by one and exports weknowhow.expect from it.

---
+++ index.js
@@ -0,0 +1,38 @@
+var Path = require('path'),
+    fs = require('fs'),
+    vm = require('vm'),
+    context = vm.createContext({
+        console: console,
+        Buffer: Buffer,
+        // Reuse the existing context's globals so instanceof checks work:
+        Object: Object,
+        Array: Array,
+        Error: Error,
+        RegExp: RegExp,
+        Date: Date,
+        String: String,
+        Number: Number,
+        Math: Math,
+        Boolean: Boolean,
+        Function: Function
+    });
+
+[
+    'unexpected-namespace.js',
+    'unexpected-es4-compatible.js',
+    'unexpected-es5-compatible.js',
+    'unexpected-utils.js',
+    'unexpected-equal.js',
+    'unexpected-inspect.js',
+    'unexpected-core.js',
+    'unexpected-assertions.js',
+    'unexpected-module.js'
+].forEach(function (fileName) {
+    try {
+        vm.runInContext(fs.readFileSync(Path.resolve(__dirname, 'src', fileName), 'utf-8'), context, fileName);
+    } catch (err) {
+        console.error('Error loading ' + fileName + ': ' + err.stack);
+    }
+});
+
+module.exports = context.weknowhow.expect;
