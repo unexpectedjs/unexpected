unexpectedjs/unexpected

commit e45009eaa55dc2ea5673dbdba5eb0e5f0892eb54

equal: Removed try..catch that doesn't seem to be necessary with utils.getKeys.
Object.keys does throw for string arguments, however, so that must be why it was there in the first place.

lib/unexpected-core.js
@@ -106,13 +106,11 @@
                 }
                 return this.equal(Array.prototype.slice.call(actual), Array.prototype.slice.call(expected), stack);
             }
-            var actualKeys, expectedKeys, key, i;
-            try {
-                actualKeys = getKeys(actual);
-                expectedKeys = getKeys(expected);
-            } catch (e) {//happens when one is a string literal and the other isn't
-                return false;
-            }
+            var actualKeys = getKeys(actual),
+                expectedKeys = getKeys(expected),
+                key,
+                i;
+
             // having the same number of owned properties (keys incorporates hasOwnProperty)
             if (actualKeys.length !== expectedKeys.length) {
                 return false;

commit f4fe7766b16972b042a4dff058377f79b4bb7c6b

equal: Avoid calling util.getKeysOfDefinedProperties on non-objects.
e45009e caused equal to fail to compare an object to a number.

lib/unexpected-core.js
@@ -107,8 +107,8 @@
                 }
                 return this.equal(Array.prototype.slice.call(actual), Array.prototype.slice.call(expected), stack);
             }
-            var actualKeys = getKeysOfDefinedProperties(actual),
-                expectedKeys = getKeysOfDefinedProperties(expected),
+            var actualKeys = (actual && typeof actual === 'object') ? getKeysOfDefinedProperties(actual) : [],
+                expectedKeys = (expected && typeof expected === 'object') ? getKeysOfDefinedProperties(expected) : [],
                 key,
                 i;
 