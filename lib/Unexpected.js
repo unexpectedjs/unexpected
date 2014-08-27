unexpectedjs/unexpected

commit dab87666d4632d0eb2a9c43352b63d316c177f06

expect: Let the user know if the assertion is defined for other types, but not the subject type.

lib/Unexpected.js
@@ -129,27 +129,6 @@ Unexpected.prototype.fail = function (arg) {
     throw error;
 };
 
-Unexpected.prototype.findAssertionSimilarTo = function (text, type) {
-    var editDistances = [];
-    while (type) {
-        forEach(getKeys(this.assertions[type.name]), function (assertion) {
-            var distance = levenshteinDistance(text, assertion);
-            editDistances.push({
-                type: type,
-                assertion: assertion,
-                distance: distance
-            });
-        });
-        type = type.baseType;
-    }
-    editDistances.sort(function (x, y) {
-        return x.distance - y.distance;
-    });
-    return map(editDistances.slice(0, 5), function (editDistance) {
-        return editDistance.assertion;
-    });
-};
-
 // addAssertion(pattern, handler)
 // addAssertion([pattern, ...]], handler)
 // addAssertion(typeName, pattern, handler)
@@ -434,13 +413,42 @@ Unexpected.prototype.expect = function expect(subject, testDescriptionString) {
             throw err;
         }
     } else {
-        var similarAssertions = this.findAssertionSimilarTo(testDescriptionString, matchingType);
-        var message =
-            'Unknown assertion "' + testDescriptionString + '", ' +
-            'did you mean: "' + similarAssertions[0] + '"';
-        var err = new Error(message);
-        truncateStack(err, this.expect);
-        throw err;
+        var errorMessage;
+        var definedForIncompatibleTypes = filter(this.types, function (type) {
+            return this.assertions[type.name][testDescriptionString];
+        }, this);
+        if (definedForIncompatibleTypes.length > 0) {
+            errorMessage =
+                'The assertion "' + testDescriptionString + '" is not defined for the type "' + matchingType.name +
+                '", but it is defined for ';
+            if (definedForIncompatibleTypes.length === 1) {
+                errorMessage += 'the type "' + definedForIncompatibleTypes[0].name + '"';
+            } else {
+                errorMessage += 'these types: ' + map(definedForIncompatibleTypes, function (incompatibleType) {
+                    return '"' + incompatibleType.name + '"';
+                }).join(', ');
+            }
+        } else {
+            var distances = [];
+            forEach(this.types, function (type) {
+                forEach(getKeys(this.assertions[type.name]), function (assertion) {
+                    var distance = levenshteinDistance(testDescriptionString, assertion);
+                    distances.push({
+                        type: type,
+                        assertion: assertion,
+                        distance: distance
+                    });
+                });
+            }, this);
+            distances.sort(function (x, y) {
+                return x.distance - y.distance;
+            });
+            errorMessage =
+                'Unknown assertion "' + testDescriptionString + '", ' +
+                'did you mean: "' + distances[0].assertion + '"';
+        }
+
+        throw truncateStack(new Error(errorMessage));
     }
 };
 

test/unexpected.spec.js
@@ -1748,14 +1748,45 @@ describe('unexpected', function () {
             }, 'to throw');
         });
 
-        it('suggests an assertion if the given assertion does not exists', function () {
-            expect(function () {
-                clonedExpect(1, "to bee", 2);
-            }, 'to throw', 'Unknown assertion "to bee", did you mean: "to be"');
+        describe('when the assertion does not exist', function () {
+            it('it suggests a similarly named assertion', function () {
+                expect(function () {
+                    clonedExpect(1, "to bee", 2);
+                }, 'to throw', 'Unknown assertion "to bee", did you mean: "to be"');
 
-            expect(function () {
-                clonedExpect(1, "to be answer to the ultimate question of life, the universe, and everything");
-            }, 'to throw', 'Unknown assertion "to be answer to the ultimate question of life, the universe, and everything", did you mean: "to be answer to the Ultimate Question of Life, the Universe, and Everything"');
+                expect(function () {
+                    clonedExpect(1, "to be answer to the ultimate question of life, the universe, and everything");
+                }, 'to throw', 'Unknown assertion "to be answer to the ultimate question of life, the universe, and everything", did you mean: "to be answer to the Ultimate Question of Life, the Universe, and Everything"');
+            });
+
+            describe('but exists for another type', function () {
+                it('explains that in the error message', function () {
+                    clonedExpect.addAssertion('array', 'to foobarquux', function (expect, subject) {
+                        expect(subject, 'to equal', ['foobarquux']);
+                    });
+                    clonedExpect(['foobarquux'], 'to foobarquux');
+                    expect(function () {
+                        clonedExpect('foobarquux', 'to foobarquux');
+                    }, 'to throw', 'The assertion "to foobarquux" is not defined for the type "string", but it is defined for the type "array"');
+                });
+
+                it('prefers to suggest a similarly named assertion defined for the correct type over an exact match defined for other types', function () {
+                    clonedExpect.addAssertion('array', 'to foo', function (expect, subject) {
+                        expect(subject, 'to equal', ['foo']);
+                    }).addAssertion('string', 'to fooo', function (expect, subject) {
+                        expect(subject, 'to equal', 'fooo');
+                    });
+                    expect(function (){
+                        clonedExpect(['fooo'], 'to fooo');
+                    }, 'to throw', 'The assertion "to fooo" is not defined for the type "array", but it is defined for the type "string"');
+                    clonedExpect.addAssertion('null', 'to fooo', function (expect, subject) {
+                        expect(subject.message, 'to equal', 'fooo');
+                    });
+                    expect(function (){
+                        clonedExpect(['fooo'], 'to fooo');
+                    }, 'to throw', 'The assertion "to fooo" is not defined for the type "array", but it is defined for these types: "null", "string"');
+                });
+            });
         });
 
         describe('toString', function () {

commit 32a64583e9416611683801d1a0e7f9524e2b07c5

Fixed truncateStack regression introduced in dab87666.

lib/Unexpected.js
@@ -449,7 +449,7 @@ Unexpected.prototype.expect = function expect(subject, testDescriptionString) {
                 'did you mean: "' + assertionsWithScore[0].assertion + '"';
         }
 
-        throw truncateStack(new Error(errorMessage));
+        throw truncateStack(new Error(errorMessage), this.expect);
     }
 };
 