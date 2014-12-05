8ff0e146834128003e9329c93acc73fdb1f1ba79
Sune Simonsen <sune@we-knowhow.dk>
2014-12-05 09:48:56 +0100

    Highlight the selected assertion in the menu

--- site/build.js
+++ site/build.js
@@ -2,6 +2,10 @@ var metalSmith = require('metalsmith');
 var Path = require('path');
 var fs = require('fs');
 
+function titleToId(title) {
+    return title.replace(/ /g, '-');
+}
+
 metalSmith(__dirname)
     .destination('../site-build')
     .source('src')
@@ -19,17 +23,18 @@ metalSmith(__dirname)
     }))
     // Dynamicly generate metadata for assertion files
     .use(function (files, metalsmith, next) {
-        function assertionTitleFromFileName(fileName) {
-            var collection = fileName.match(/^assertions\/([^\/]+)/)[1];
-            var assertionName = fileName.match(/([^\/]+)\.md$/)[1];
-            assertionName = assertionName.replace(/-/g, ' ');
-            return '[' + collection + '] ' + assertionName;
-        }
         Object.keys(files).forEach(function (file) {
             if (files[file].collection.indexOf('assertions') !== -1) {
                 // Set the template of all documents in the assertion collection
+
+                var type = file.match(/^assertions\/([^\/]+)/)[1];
+                var assertionName = file.match(/([^\/]+)\.md$/)[1];
+                assertionName = titleToId(assertionName);
+
                 files[file].template = 'assertion.ejs';
-                files[file].title = assertionTitleFromFileName(file);
+                files[file].name = assertionName;
+                files[file].title = type + ' - ' + assertionName;
+                files[file].type = type;
             }
         });
         next();
@@ -49,8 +54,11 @@ metalSmith(__dirname)
         var metadata = metalsmith.metadata();
         // Set globally available meta data here
         // metadata.title = 'Unexpected.js';
-        metadata.titleToId = function titleToId(title) {
-            return title.replace(/ /g, '-');
+        metadata.titleToId = titleToId;
+        metadata.isActiveAssertion = function (path, type, assertionName) {
+            var currentType = path.match(/^assertions\/([^\/]+)/);
+            var currentAssertionName = path.match(/([^\/]+)\/?$/);
+            return currentType && currentAssertionName && titleToId(type) === currentType[1] && titleToId(assertionName) === currentAssertionName[1];
         };
         fs.readFile(Path.resolve(__dirname, 'src','_data.json'), 'utf-8', function (err, data) {
             if (err) {
