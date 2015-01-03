3ab5f39f9a4c133ac44a960bb478038e98a68fae
Sune Simonsen <sune@we-knowhow.dk>
2015-01-03 14:50:37 +0100

    Moved to match to the string section

--- test/documentation.spec.js
+++ test/documentation.spec.js
@@ -301,36 +301,6 @@ describe('assertions/any/to-equal.md', function () {
         ].join('\n'));
     });
 });
-describe('assertions/any/to-match.md', function () {
-    it('#1', function () {
-        expect(function () {
-            expect('Hello beautiful world!', 'to match', /bea.t.*/);
-        }, 'not to throw');
-    });
-    it('#2', function () {
-        expect(function () {
-            expect('Hello world!', 'to match', /beautiful/);
-        }, 'to throw', [
-            'expected \'Hello world!\' to match /beautiful/'
-        ].join('\n'));
-    });
-    it('#3', function () {
-        expect(function () {
-            expect('Hello world!', 'not to match', /beautiful/);
-        }, 'not to throw');
-    });
-    it('#4', function () {
-        expect(function () {
-            expect('Hello beautiful world!', 'not to match', /beautiful/);
-            expect({ foo: 'foo' }, 'not to match', /foo/);
-            expect(null, 'not to match', /foo/);
-        }, 'to throw', [
-            'expected \'Hello beautiful world!\' not to match /beautiful/',
-            '',
-            'Hello beautiful world!'
-        ].join('\n'));
-    });
-});
 describe('assertions/boolean/to-be-false.md', function () {
     it('#1', function () {
         expect(function () {
@@ -412,6 +382,34 @@ describe('assertions/string/to-have-length.md', function () {
             expect('Hello world', 'not to have length', 11);
         }, 'to throw', [
             'expected \'Hello world\' not to have length 11'
+        ].join('\n'));
+    });
+});
+describe('assertions/string/to-match.md', function () {
+    it('#1', function () {
+        expect(function () {
+            expect('Hello beautiful world!', 'to match', /bea.t.*/);
+        }, 'not to throw');
+    });
+    it('#2', function () {
+        expect(function () {
+            expect('Hello world!', 'to match', /beautiful/);
+        }, 'to throw', [
+            'expected \'Hello world!\' to match /beautiful/'
+        ].join('\n'));
+    });
+    it('#3', function () {
+        expect(function () {
+            expect('Hello world!', 'not to match', /beautiful/);
+        }, 'not to throw');
+    });
+    it('#4', function () {
+        expect(function () {
+            expect('Hello beautiful world!', 'not to match', /beautiful/);
+        }, 'to throw', [
+            'expected \'Hello beautiful world!\' not to match /beautiful/',
+            '',
+            'Hello beautiful world!'
         ].join('\n'));
     });
 });
