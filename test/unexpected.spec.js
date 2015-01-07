2e50deeb3ef729537f3a3b1a326c207cbce2eaf9
Sune Simonsen <sune@we-knowhow.dk>
2015-01-07 20:53:18 +0100

    Fixed bug where expect({ foo: 'b'}, 'not to have keys', []) would fail

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -1114,6 +1114,7 @@ describe('unexpected', function () {
         it('asserts the presence of a key', function () {
             expect({ a: 'b' }, 'to have key', 'a');
             expect({ a: 'b' }, 'not to have key', 'b');
+            expect({ a: 'b' }, 'not to have key', []);
             expect({ a: 'b', c: 'd' }, 'not to have key', 'b');
             expect({ a: 'b', c: 'd' }, 'to not only have key', 'a');
             expect({ a: 'b', c: 'd' }, 'to only have keys', 'a', 'c');
