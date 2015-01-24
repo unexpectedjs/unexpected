2d63ad189ea3a7327f0f300c8ff8f81f2814418b
Sune Simonsen <sune@we-knowhow.dk>
2015-01-24 23:48:12 +0100

    Documented to have properties

--- test/documentation.spec.js
+++ test/documentation.spec.js
@@ -827,6 +827,59 @@ describe('assertions/object/to-be-canonical.md', function () {
         ].join('\n'));
     });
 });
+describe('assertions/object/to-have-properties.md', function () {
+    it('#1', function () {
+        expect(function () {
+            expect({ a: 'a', b: { c: 'c' }, d: 'd' }, 'to have properties', ['a', 'b']);
+            expect({ a: 'a', b: { c: 'c' }, d: 'd' }, 'to have properties', {
+                a: 'a',
+                b: { c: 'c' }
+            });
+            expect([ 'a', { c: 'c' }, 'd' ], 'to have properties', {
+                1: { c: 'c' },
+                2: 'd'
+            });
+        }, 'not to throw');
+    });
+    it('#2', function () {
+        expect(function () {
+            expect({ a: 'a', b: { c: 'c' }, d: 'd' }, 'to have own properties', ['a', 'b']);
+            expect({ a: 'a', b: { c: 'c' }, d: 'd' }, 'to have own properties', {
+                a: 'a',
+                b: { c: 'c' }
+            });
+        }, 'not to throw');
+    });
+    it('#3', function () {
+        expect(function () {
+            expect({ a: 'f00', b: 'bar' }, 'to have own properties', {
+              a: 'foo'
+            });
+        }, 'to throw', [
+            'expected { a: \'f00\', b: \'bar\' } to have own properties { a: \'foo\' }',
+            '',
+            '{',
+            '  a: \'f00\', // should be \'foo\'',
+            '            // -f00',
+            '            // +foo',
+            '  b: \'bar\'',
+            '}'
+        ].join('\n'));
+    });
+    it('#4', function () {
+        expect(function () {
+            expect({ a: 'a', b: { c: 'c' }, d: 'd' }, 'not to have properties', ['k', 'l']);
+            expect(Object.create({ a: 'a', b: 'b' }), 'not to have own properties', ['a', 'b']);
+        }, 'not to throw');
+    });
+    it('#5', function () {
+        expect(function () {
+            expect({ a: 'a', b: { c: 'c' }, d: 'd' }, 'not to have properties', ['b', 'd']);
+        }, 'to throw', [
+            'expected { a: \'a\', b: { c: \'c\' }, d: \'d\' } not to have properties [ \'b\', \'d\' ]'
+        ].join('\n'));
+    });
+});
 describe('assertions/object/to-have-property.md', function () {
     it('#1', function () {
         expect(function () {
