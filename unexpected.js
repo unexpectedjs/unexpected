d97b65063fe84af6ecfafeb7d5cb38bee7b42d0b
Sune Simonsen <sune@we-knowhow.dk>
2015-05-12 16:24:19 +0200

    Build unexpected.js

--- unexpected.js
+++ unexpected.js
@@ -754,7 +754,7 @@ Unexpected.prototype.expect = function expect(subject, testDescriptionString) {
             wrappedExpect.diff = that.diff;
             wrappedExpect.findTypeOf = that.findTypeOf.bind(that);
             wrappedExpect.findCommonType = that.findCommonType.bind(that);
-            wrappedExpect.getType = expect.getType;
+            wrappedExpect.getType = that.getType;
             wrappedExpect.output = that.output;
             wrappedExpect.outputFormat = that.outputFormat;
             wrappedExpect.fail = function () {
@@ -1941,13 +1941,13 @@ module.exports = function (expect) {
         }
     });
 
-    function wrapDiffWithTypePrefixAndSuffix(e, type) {
+    function wrapDiffWithTypePrefixAndSuffix(e, type, subject) {
         var createDiff = e.createDiff;
         if (createDiff) {
             return function (output) { // ...
-                type.prefix.call(e, output);
+                type.prefix.call(type, output, subject);
                 var result = createDiff.apply(this, arguments);
-                type.suffix.call(e, output);
+                type.suffix.call(type, output, subject);
                 return result;
             };
         }
@@ -1963,19 +1963,18 @@ module.exports = function (expect) {
             }, function (e) {
                 expect.fail({
                     label: e.label,
-                    diff: wrapDiffWithTypePrefixAndSuffix(e, type)
+                    diff: wrapDiffWithTypePrefixAndSuffix(e, type, subject)
                 });
             });
         } else {
             var subjectType = expect.findTypeOf(subject);
-            expect(subjectType.is('wrapperObject'), 'to be truthy');
 
             return expect.withError(function () {
                 return expect(subjectType.unwrap(subject), 'to [exhaustively] satisfy', value);
             }, function (e) {
                 expect.fail({
                     label: 'should satisfy',
-                    diff: wrapDiffWithTypePrefixAndSuffix(e, subjectType)
+                    diff: wrapDiffWithTypePrefixAndSuffix(e, subjectType, subject)
                 });
             });
         }
@@ -2891,7 +2890,7 @@ module.exports = function (expect) {
         inspect: function (value, depth, output, inspect) {
             var errorObject = this.unwrap(value);
             // TODO: Inspect Error as a built-in once we have the styles defined:
-            output.text((value.constructor && value.constructor.name || 'Error') + '(').append(inspect(errorObject, depth)).text(')');
+            output.text((value.name || value.constructor && value.constructor.name  || 'Error') + '(').append(inspect(errorObject, depth)).text(')');
         },
         diff: function (actual, expected, output, diff) {
             var result = diff(this.unwrap(actual), this.unwrap(expected));
@@ -3546,7 +3545,7 @@ module.exports = function arrayChanges(actual, expected, equal, similar) {
         return false;
     };
 
-    var itemsDiff = arrayDiff(actual, expected, function (a, b) {
+    var itemsDiff = arrayDiff([].concat(actual), [].concat(expected), function (a, b) {
         return equal(a, b) || similar(a, b);
     });
 
@@ -3572,7 +3571,7 @@ module.exports = function arrayChanges(actual, expected, equal, similar) {
 
     function updateRemoveTable() {
         removedItems = 0;
-        actual.forEach(function (_, index) {
+        Array.prototype.forEach.call(actual, function (_, index) {
             removedItems += removesByIndex[index] || 0;
             removeTable[index] = removedItems;
         });
@@ -10142,7 +10141,7 @@ var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 }(typeof exports === 'undefined' ? (this.base64js = {}) : exports))
 
 },{}],19:[function(require,module,exports){
-exports.read = function (buffer, offset, isLE, mLen, nBytes) {
+exports.read = function(buffer, offset, isLE, mLen, nBytes) {
   var e, m,
       eLen = nBytes * 8 - mLen - 1,
       eMax = (1 << eLen) - 1,
@@ -10150,32 +10149,32 @@ exports.read = function (buffer, offset, isLE, mLen, nBytes) {
       nBits = -7,
       i = isLE ? (nBytes - 1) : 0,
       d = isLE ? -1 : 1,
-      s = buffer[offset + i]
+      s = buffer[offset + i];
 
-  i += d
+  i += d;
 
-  e = s & ((1 << (-nBits)) - 1)
-  s >>= (-nBits)
-  nBits += eLen
-  for (; nBits > 0; e = e * 256 + buffer[offset + i], i += d, nBits -= 8) {}
+  e = s & ((1 << (-nBits)) - 1);
+  s >>= (-nBits);
+  nBits += eLen;
+  for (; nBits > 0; e = e * 256 + buffer[offset + i], i += d, nBits -= 8);
 
-  m = e & ((1 << (-nBits)) - 1)
-  e >>= (-nBits)
-  nBits += mLen
-  for (; nBits > 0; m = m * 256 + buffer[offset + i], i += d, nBits -= 8) {}
+  m = e & ((1 << (-nBits)) - 1);
+  e >>= (-nBits);
+  nBits += mLen;
+  for (; nBits > 0; m = m * 256 + buffer[offset + i], i += d, nBits -= 8);
 
   if (e === 0) {
-    e = 1 - eBias
+    e = 1 - eBias;
   } else if (e === eMax) {
-    return m ? NaN : ((s ? -1 : 1) * Infinity)
+    return m ? NaN : ((s ? -1 : 1) * Infinity);
   } else {
-    m = m + Math.pow(2, mLen)
-    e = e - eBias
+    m = m + Math.pow(2, mLen);
+    e = e - eBias;
   }
-  return (s ? -1 : 1) * m * Math.pow(2, e - mLen)
-}
+  return (s ? -1 : 1) * m * Math.pow(2, e - mLen);
+};
 
-exports.write = function (buffer, value, offset, isLE, mLen, nBytes) {
+exports.write = function(buffer, value, offset, isLE, mLen, nBytes) {
   var e, m, c,
       eLen = nBytes * 8 - mLen - 1,
       eMax = (1 << eLen) - 1,
@@ -10183,49 +10182,49 @@ exports.write = function (buffer, value, offset, isLE, mLen, nBytes) {
       rt = (mLen === 23 ? Math.pow(2, -24) - Math.pow(2, -77) : 0),
       i = isLE ? 0 : (nBytes - 1),
       d = isLE ? 1 : -1,
-      s = value < 0 || (value === 0 && 1 / value < 0) ? 1 : 0
+      s = value < 0 || (value === 0 && 1 / value < 0) ? 1 : 0;
 
-  value = Math.abs(value)
+  value = Math.abs(value);
 
   if (isNaN(value) || value === Infinity) {
-    m = isNaN(value) ? 1 : 0
-    e = eMax
+    m = isNaN(value) ? 1 : 0;
+    e = eMax;
   } else {
-    e = Math.floor(Math.log(value) / Math.LN2)
+    e = Math.floor(Math.log(value) / Math.LN2);
     if (value * (c = Math.pow(2, -e)) < 1) {
-      e--
-      c *= 2
+      e--;
+      c *= 2;
     }
     if (e + eBias >= 1) {
-      value += rt / c
+      value += rt / c;
     } else {
-      value += rt * Math.pow(2, 1 - eBias)
+      value += rt * Math.pow(2, 1 - eBias);
     }
     if (value * c >= 2) {
-      e++
-      c /= 2
+      e++;
+      c /= 2;
     }
 
     if (e + eBias >= eMax) {
-      m = 0
-      e = eMax
+      m = 0;
+      e = eMax;
     } else if (e + eBias >= 1) {
-      m = (value * c - 1) * Math.pow(2, mLen)
-      e = e + eBias
+      m = (value * c - 1) * Math.pow(2, mLen);
+      e = e + eBias;
     } else {
-      m = value * Math.pow(2, eBias - 1) * Math.pow(2, mLen)
-      e = 0
+      m = value * Math.pow(2, eBias - 1) * Math.pow(2, mLen);
+      e = 0;
     }
   }
 
-  for (; mLen >= 8; buffer[offset + i] = m & 0xff, i += d, m /= 256, mLen -= 8) {}
+  for (; mLen >= 8; buffer[offset + i] = m & 0xff, i += d, m /= 256, mLen -= 8);
 
-  e = (e << mLen) | m
-  eLen += mLen
-  for (; eLen > 0; buffer[offset + i] = e & 0xff, i += d, e /= 256, eLen -= 8) {}
+  e = (e << mLen) | m;
+  eLen += mLen;
+  for (; eLen > 0; buffer[offset + i] = e & 0xff, i += d, e /= 256, eLen -= 8);
 
-  buffer[offset + i - d] |= s * 128
-}
+  buffer[offset + i - d] |= s * 128;
+};
 
 },{}],20:[function(require,module,exports){
 
