unexpectedjs/unexpected

commit bfc9eedda1b71a327dc4339188ccfd98b4597f66

Fixed inspection of the constructor name for Error subclasses.

lib/types.js
@@ -505,7 +505,7 @@ module.exports = function (expect) {
         },
         inspect: function (value, depth, output, inspect) {
             // TODO: Inspect Error as a built-in once we have the styles defined:
-            output.text((value.name || value.constructor && value.constructor.name || 'Error') + '(');
+            output.text(((value.constructor && value.constructor.name) || value.name || 'Error') + '(');
             var keys = this.getKeys(value);
             if (keys.length === 1 && keys[0] === 'message') {
                 if (value.message !== '') {

test/unexpected.spec.js
@@ -978,6 +978,45 @@ describe('unexpected', function () {
             expect(err, 'to inspect as', "Error({ message: '', bar: 123 })");
         });
 
+        describe('with a custom Error class inheriting from Error', function () {
+            function inherits(ctor, superCtor) {
+                ctor.super_ = superCtor;
+                ctor.prototype = Object.create(superCtor.prototype, {
+                    constructor: {
+                        value: ctor,
+                        enumerable: false,
+                        writable: true,
+                        configurable: true
+                    }
+                });
+            }
+
+            function MyError(message) {
+                Error.call(this);
+                this.message = message;
+            }
+
+            inherits(MyError, Error);
+
+            it('should consider identical instances to be identical', function () {
+                expect(new MyError('foo'), 'to equal', new MyError('foo'));
+            });
+
+            it('should instances of the custom error different to be different when they have different messages', function () {
+                expect(function () {
+                    expect(new MyError('foo'), 'to equal', new MyError('bar'));
+                }, 'to throw',
+                    "expected MyError('foo') to equal MyError('bar')\n" +
+                    "\n" +
+                    "MyError({\n" +
+                    "  message: 'foo' // should equal 'bar'\n" +
+                    "                 // -foo\n" +
+                    "                 // +bar\n" +
+                    "})"
+                );
+            });
+        });
+
         describe('to have message assertion', function () {
             describe('with an Unexpected error', function () {
                 var err;

commit 58e138baee10b2c7d489399406d53f71815d5073

Let the 'name' property take precedence again when inspecting Error instances.
Fixes a regression introduced in bfc9eedda.

lib/styles.js
@@ -97,4 +97,14 @@ module.exports = function (expect) {
     expect.addStyle('shouldEqualError', function (expected, inspect) {
         this.error(typeof expected === 'undefined' ? 'should be' : 'should equal').sp().block(inspect(expected));
     });
+
+    expect.addStyle('errorName', function (error) {
+        if (typeof error.name === 'string' && error.name !== 'Error') {
+            this.text(error.name);
+        } else if (error.constructor && typeof error.constructor.name === 'string') {
+            this.text(error.constructor.name);
+        } else {
+            this.text('Error');
+        }
+    });
 };

lib/types.js
@@ -504,8 +504,7 @@ module.exports = function (expect) {
                 (equal(a.message, b.message) && this.baseType.equal(a, b));
         },
         inspect: function (value, depth, output, inspect) {
-            // TODO: Inspect Error as a built-in once we have the styles defined:
-            output.text(((value.constructor && value.constructor.name) || value.name || 'Error') + '(');
+            output.errorName(value).text('(');
             var keys = this.getKeys(value);
             if (keys.length === 1 && keys[0] === 'message') {
                 if (value.message !== '') {
@@ -518,12 +517,18 @@ module.exports = function (expect) {
         },
         diff: function (actual, expected, output, diff) {
             if (actual.constructor !== expected.constructor) {
-                return this.baseType.diff(actual, expected, output, diff);
+                return {
+                    diff: output.text('Mismatching constructors ')
+                        .errorName(actual)
+                        .text(' should be ').errorName(expected),
+                    inline: false
+                };
             }
 
             var result = diff(this.unwrap(actual), this.unwrap(expected));
             if (result.diff) {
-                result.diff = utils.wrapConstructorNameAroundOutput(result.diff, actual);
+                result.diff = output.clone().errorName(actual).text('(').append(result.diff).text(')');
+
             }
             return result;
         }

test/unexpected.spec.js
@@ -1025,6 +1025,41 @@ describe('unexpected', function () {
                     "})"
                 );
             });
+
+            describe('when the custom error has a "name" property', function () {
+                var myError = new MyError('foo');
+                myError.name = 'SomethingElse';
+
+                it('should use the "name" property when inspecting instances', function () {
+                    expect(myError, 'to inspect as', "SomethingElse('foo')");
+                });
+
+                it('should use the "name" property when reporting mismatching constructors', function () {
+                    expect(function () {
+                        expect(myError, 'to equal', new Error('foo'));
+                    }, 'to throw',
+                        "expected SomethingElse('foo') to equal Error('foo')\n" +
+                        "\n" +
+                        "Mismatching constructors SomethingElse should be Error"
+                    );
+                });
+
+                it('should use the "name" property when diffing', function () {
+                    expect(function () {
+                        var otherMyError = new MyError('bar');
+                        otherMyError.name = 'SomethingElse';
+                        expect(myError, 'to equal', otherMyError);
+                    }, 'to throw',
+                        "expected SomethingElse('foo') to equal SomethingElse('bar')\n" +
+                        "\n" +
+                        "SomethingElse({\n" +
+                        "  message: 'foo' // should equal 'bar'\n" +
+                        "                 // -foo\n" +
+                        "                 // +bar\n" +
+                        "})"
+                    );
+                });
+            });
         });
 
         describe('to have message assertion', function () {