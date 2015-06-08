bf38872c82490cb9df45bbdfa1befca78beacc88
Andreas Lind <andreas@one.com>
2015-06-08 10:52:15 +0200

    Date type: Inspect the milliseconds when non-zero. Fixes #167.

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -1120,6 +1120,24 @@ describe('unexpected', function () {
         });
     });
 
+    describe('Date type', function () {
+        it('inspects without milliseconds when the milliseconds field is zero', function () {
+            expect(new Date(0), 'to inspect as', "new Date('Thu, 01 Jan 1970 00:00:00 GMT')");
+        });
+
+        it('inspects with three milliseconds digits when the milliseconds field has one digit', function () {
+            expect(new Date(1), 'to inspect as', "new Date('Thu, 01 Jan 1970 00:00:00.001 GMT')");
+        });
+
+        it('inspects with three milliseconds digits when the milliseconds field has two digits', function () {
+            expect(new Date(10), 'to inspect as', "new Date('Thu, 01 Jan 1970 00:00:00.010 GMT')");
+        });
+
+        it('inspects with three milliseconds digits when the milliseconds field has three digits', function () {
+            expect(new Date(100), 'to inspect as', "new Date('Thu, 01 Jan 1970 00:00:00.100 GMT')");
+        });
+    });
+
     describe('arity assertion', function () {
         it('tests that the subject function has the given arity', function () {
             expect(function () {}, 'to have arity', 0);
