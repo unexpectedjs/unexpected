800a9448baab3c1db1ea21905b900cbbf9c3b2c1
Andreas Lind <andreas@one.com>
2015-06-15 16:36:43 +0200

    Added custom inspect function for UnexpectedError.
    It now simply inspects as UnexpectedError(<message>).
    
    Multiline messages are inspected as:
    
    UnexpectedError(
        <first line of message>...
        <second line of message>
        ...
    )

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -1149,7 +1149,7 @@ describe('unexpected', function () {
                                 "  -expected 1 to equal 2\n" +
                                 "  +expected 3 to equal 2"
                         );
-                        expect(message, 'to match', /^expected\sUnexpectedError\(\{[\s\S]*\}\)/);
+                        expect(message, 'to match', /^expected\sUnexpectedError\([\s\S]*\)/);
                     });
                 });
             });
@@ -1169,6 +1169,37 @@ describe('unexpected', function () {
                         "\n" +
                         "  -Bummer!\n" +
                         "  +Dammit!"
+                    );
+                });
+            });
+        });
+    });
+
+    describe('UnexpectedError', function () {
+        describe('with a single line message', function () {
+            it('should be inspected correctly', function () {
+                expect(function () {
+                    expect(2, 'to equal', 4);
+                }, 'to throw', function (err) {
+                    expect(err, 'to inspect as', 'UnexpectedError(expected 2 to equal 4)');
+                });
+            });
+        });
+
+        describe('with a multiline message', function () {
+            it('should be inspected correctly', function () {
+                expect(function () {
+                    expect('foo', 'to equal', 'bar');
+                }, 'to throw', function (err) {
+                    expect(
+                        err,
+                        'to inspect as',
+                        "UnexpectedError(\n" +
+                        "  expected 'foo' to equal 'bar'\n" +
+                        "\n" +
+                        "  -foo\n" +
+                        "  +bar\n" +
+                        ")"
                     );
                 });
             });
