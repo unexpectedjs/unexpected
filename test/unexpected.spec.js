a2255b7ac6a44a150f16cc9db630e67f051b648e
Andreas Lind <andreas@one.com>
2015-08-02 12:16:43 +0200

    Inspect magicpen instances as a series of function calls instead of simply appending its value.
    I ran into troubles with testing error messages because the pen instances being tested were appended to the output despite being in a different format.

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -72,6 +72,63 @@ describe('unexpected', function () {
         });
     });
 
+    describe('magicpen type', function () {
+        describe('#inspect', function () {
+            describe('with a pen in text format', function () {
+                var pen = expect.createOutput('text').green('abc').nl().text('def').block(function () {
+                    this.text('foo').nl().text('bar');
+                });
+
+                it('should inspect correctly', function () {
+                    expect(pen, 'to inspect as',
+                        "magicpen('text')        // abc\n" +
+                        "  .green('abc').nl()    // deffoo\n" +
+                        "  .text('def')          //    bar\n" +
+                        "  .block(function () {\n" +
+                        "    this\n" +
+                        "      .text('foo').nl()\n" +
+                        "      .text('bar');\n" +
+                        "  })"
+                    );
+                });
+            });
+
+            describe('with a pen in ansi format', function () {
+                var pen = expect.createOutput('ansi').green('abc').text('def').block(function () {
+                    this.text('foo');
+                });
+
+                it('should inspect correctly', function () {
+                    expect(pen, 'to inspect as',
+                        "magicpen('ansi')\n" +
+                        "  .green('abc')\n" +
+                        "  .text('def')\n" +
+                        "  .block(function () {\n" +
+                        "    this.text('foo');\n" +
+                        "  })"
+                    );
+                });
+            });
+
+            describe('with a pen in ansi format', function () {
+                var pen = expect.createOutput('html').green('abc').text('def').block(function () {
+                    this.text('foo');
+                });
+
+                it('should inspect correctly', function () {
+                    expect(pen, 'to inspect as',
+                        "magicpen('html')\n" +
+                        "  .green('abc')\n" +
+                        "  .text('def')\n" +
+                        "  .block(function () {\n" +
+                        "    this.text('foo');\n" +
+                        "  })"
+                    );
+                });
+            });
+        });
+    });
+
     describe('ok/truthy/falsy assertion', function () {
         it('assert that the value is truthy or not', function () {
             expect(1, 'to be ok');
