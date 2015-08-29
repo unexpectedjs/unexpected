22545c23cfa3ab2e8704c7c08cc8323cc54dd3de
Sune Simonsen <sune@we-knowhow.dk>
2015-08-29 21:05:36 +0200

    Added a few more benchmark tests

--- test/benchmark.spec.js
+++ test/benchmark.spec.js
@@ -6,12 +6,48 @@ describe('benchmark', function () {
         }, 0);
     }
 
-    it('to be on numbers', function () {
-        expect(42, "to be", 42);
+    describe('to be', function () {
+        it('on numbers', function () {
+            expect(42, "to be", 42);
+        });
+
+        it('on strings', function () {
+            expect(42, "to be", 42);
+        });
+
+        it('on same object', function () {
+            var obj = { label: 'this is an object', list: [ 0, 1, 2]};
+            expect(obj, "to be", obj);
+        });
     });
 
-    it('to equal on strings', function () {
-        expect("foo", "to equal", "foo");
+    describe('to equal', function () {
+        it('on numbers', function () {
+            expect(42, "to equal", 42);
+        });
+
+        it('on strings', function () {
+            expect("foo", "to equal", "foo");
+        });
+
+        it('on objects', function () {
+            expect({ label: 'this is an object', list: [ 0, 1, 2]},
+                   "to equal",
+                   { label: 'this is an object', list: [ 0, 1, 2]});
+        });
+
+        it('on same object', function () {
+            var obj = { label: 'this is an object', list: [ 0, 1, 2]};
+            expect(obj, "to equal", obj);
+        });
+    });
+
+    describe('to satisfy', function () {
+        it('on objects', function () {
+            expect({ label: 'this is an object', list: [ 0, 1, 2]},
+                   "to satisfy",
+                   { label: /object/, list: { 0: 0 }});
+        });
     });
 
     it('to call the callback async', function () {
