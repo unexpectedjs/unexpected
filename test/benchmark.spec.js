80842da6b8d5ca92978f6eda61cacfbd5b5b662f
Sune Simonsen <sune@we-knowhow.dk>
2015-08-29 15:06:21 +0200

    Added benchmark test for to call the callback async

--- test/benchmark.spec.js
+++ test/benchmark.spec.js
@@ -1,11 +1,20 @@
-var expect = require('unexpected');
-
 describe('benchmark', function () {
-    it('expect(42, "to be", 42)', function () {
+    var expect = unexpected.clone();
+    function asyncTestFunction(cb) {
+        setTimeout(function () {
+            cb();
+        }, 0);
+    }
+
+    it('to be on numbers', function () {
         expect(42, "to be", 42);
     });
 
-    it('expect("foo", "to equal", "foo")', function () {
+    it('to equal on strings', function () {
         expect("foo", "to equal", "foo");
+    });
+
+    it('to call the callback async', function () {
+        return expect(asyncTestFunction, 'to call the callback');
     });
 });
