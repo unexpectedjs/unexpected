e9392f1f6e10a2249c24f76afcf1a706fdbb334e
Andreas Lind <andreas@one.com>
2015-09-14 00:02:26 +0200

    Test: Fully cover the object diff.

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -1018,6 +1018,49 @@ describe('unexpected', function () {
         });
     });
 
+    describe('object type', function () {
+        describe('#diff', function () {
+            it('should show identical multiline values correctly in diffs', function () {
+                var clonedExpect = expect.clone().addType({
+                    name: 'numberNine',
+                    identify: function (obj) {
+                        return obj === 9;
+                    },
+                    inspect: function (value, depth, output) {
+                        output.block(function () {
+                            this.text('NUMBER').nl().text(' NINE ');
+                        });
+                    }
+                });
+                expect(function () {
+                    clonedExpect({a: 123, b: 9}, 'to equal', {a: 456, b: 9});
+                }, 'to throw',
+                    'expected\n' +
+                    '{\n' +
+                    '  a: 123,\n' +
+                    '  b:\n' +
+                    '    NUMBER\n' +
+                    '     NINE \n' +
+                    '}\n' +
+                    'to equal\n' +
+                    '{\n' +
+                    '  a: 456,\n' +
+                    '  b:\n' +
+                    '    NUMBER\n' +
+                    '     NINE \n' +
+                    '}\n' +
+                    '\n' +
+                    '{\n' +
+                    '  a: 123, // should equal 456\n' +
+                    '  b:\n' +
+                    '    NUMBER\n' +
+                    '     NINE \n' +
+                    '}'
+                );
+            });
+        });
+    });
+
     describe('Error type', function () {
         it('should inspect the constructor name correctly', function () {
             expect(new TypeError('foo'), 'to inspect as', "TypeError('foo')");
