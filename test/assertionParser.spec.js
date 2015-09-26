c43a3da6f82a23bf3af3f9b9ef4c1ef2ec293120
Sune Simonsen <sune@we-knowhow.dk>
2015-09-26 22:36:48 +0200

    Refactored assertion parser test

--- test/assertionParser.spec.js
+++ test/assertionParser.spec.js
@@ -1,14 +1,13 @@
 /*global unexpected*/
 describe('parseAssertion', function () {
     var expect = unexpected.clone();
-    describe('when given a valid assertion string', function () {
-        it('extracts the information into an object tree', function () {
-            var assertion = expect.parseAssertion('<string|function> [not] to be <string> <...string|object>');
-            expect(assertion, 'to satisfy', {
-                subject: [ { name: 'string' }, { name: 'function' } ],
-                assertion: '[not] to be',
-                args: [ [ { name: 'string' } ], [ { varargs: { name: 'string' } }, { name: 'object' } ] ]
-            });
+
+    it('converts an assertion string to an object representation', function () {
+        var assertion = expect.parseAssertion('<string|function> [not] to be <string> <...string|object>');
+        expect(assertion, 'to satisfy', {
+            subject: [ { name: 'string' }, { name: 'function' } ],
+            assertion: '[not] to be',
+            args: [ [ { name: 'string' } ], [ { varargs: { name: 'string' } }, { name: 'object' } ] ]
         });
     });
 });
