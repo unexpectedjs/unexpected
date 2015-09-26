cab9f12083d3f3d7d407379bbda2ad41e5dc642b
Sune Simonsen <sune@we-knowhow.dk>
2015-09-26 22:21:10 +0200

    Moved assertion parsing to the unexpected prototype

--- test/assertionParser.spec.js
+++ test/assertionParser.spec.js
@@ -1,11 +1,10 @@
 /*global unexpected*/
-var assertionParser = require('../lib/assertionParser');
-
-describe('assertionParser', function () {
+var Unexpected = require('../lib/Unexpected');
+describe('parseAssertion', function () {
     var expect = unexpected.clone();
     describe('when given a valid assertion string', function () {
         it('extracts the information into an object tree', function () {
-            var assertion = assertionParser('<string|function> [not] to be <string> <...string|object>');
+            var assertion = Unexpected.prototype.parseAssertion('<string|function> [not] to be <string> <...string|object>');
             expect(assertion, 'to equal', {
                 subject: [ 'string', 'function' ],
                 assertion: '[not] to be',
