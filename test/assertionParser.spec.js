5ca03c6d6662f946634d9efcf33f19e6608ca0df
Sune Simonsen <sune@we-knowhow.dk>
2015-09-26 22:32:57 +0200

    Embed real types inside the parsed assertion

--- test/assertionParser.spec.js
+++ test/assertionParser.spec.js
@@ -1,14 +1,13 @@
 /*global unexpected*/
-var Unexpected = require('../lib/Unexpected');
 describe('parseAssertion', function () {
     var expect = unexpected.clone();
     describe('when given a valid assertion string', function () {
         it('extracts the information into an object tree', function () {
-            var assertion = Unexpected.prototype.parseAssertion('<string|function> [not] to be <string> <...string|object>');
-            expect(assertion, 'to equal', {
-                subject: [ 'string', 'function' ],
+            var assertion = expect.parseAssertion('<string|function> [not] to be <string> <...string|object>');
+            expect(assertion, 'to satisfy', {
+                subject: [ { name: 'string' }, { name: 'function' } ],
                 assertion: '[not] to be',
-                args: [ [ 'string' ], [ { varargs: 'string' }, 'object' ] ]
+                args: [ [ { name: 'string' } ], [ { varargs: { name: 'string' } }, { name: 'object' } ] ]
             });
         });
     });
