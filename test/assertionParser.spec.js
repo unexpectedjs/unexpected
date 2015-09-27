1b14430ed1fee39748ffabfdb03670b762c5e300
Andreas Lind <andreas@one.com>
2015-09-27 23:53:50 +0200

    Change varargs syntax from ...foo to foo* and also support foo+ and foo?
    Also change the format of the return value of parseAssertion from
    { varargs: <type> } to { minimum: <number>, maximum: <number>, type: <type> }
    and remove the non-varargs special casing.

--- test/assertionParser.spec.js
+++ test/assertionParser.spec.js
@@ -3,27 +3,30 @@ describe('parseAssertion', function () {
     var expect = unexpected.clone();
 
     it('converts an assertion string to an object representation', function () {
-        var assertion = expect.parseAssertion('<string|function> [not] to be <string> <...string|object>');
+        var assertion = expect.parseAssertion('<string|function> [not] to be <string> <string*|object>');
         expect(assertion, 'to satisfy', {
-            subject: [ { name: 'string' }, { name: 'function' } ],
+            subject: [ { type: { name: 'string' }, minimum: 1, maximum: 1 }, { type: { name: 'function' }, minimum: 1, maximum: 1 } ],
             assertion: '[not] to be',
-            args: [ [ { name: 'string' } ], [ { varargs: { name: 'string' } }, { name: 'object' } ] ]
+            args: [
+                [ { type: { name: 'string' }, minimum: 1, maximum: 1 } ],
+                [ { type: { name: 'string' }, minimum: 0, maximum: Infinity }, { type: { name: 'object' }, minimum: 1, maximum: 1 } ]
+            ]
         });
     });
 
     it('accepts legacy assertions', function () {
         var assertion = expect.parseAssertion('[not] to be');
         expect(assertion, 'to satisfy', {
-            subject: [ { name: 'any' } ],
+            subject: [ { type: { name: 'any' }, minimum: 1, maximum: 1 } ],
             assertion: '[not] to be',
-            args: [ [ { varargs: { name: 'any' } } ] ]
+            args: [ [ { type: { name: 'any' }, minimum: 0, maximum: Infinity } ] ]
         });
     });
 
     it('accepts assertions with no arguments', function () {
         var assertion = expect.parseAssertion('<any> [not] to be truthy');
         expect(assertion, 'to satisfy', {
-            subject: [ { name: 'any' } ],
+            subject: [ { type: { name: 'any' }, minimum: 1, maximum: 1 } ],
             assertion: '[not] to be truthy',
             args: []
         });
@@ -31,31 +34,31 @@ describe('parseAssertion', function () {
 
     it('throw a type cannot be detected', function () {
         expect(function () {
-            expect.parseAssertion('<string|function> [not] to be <string> <...foo|object>');
-        }, 'to throw', 'Unknown type: foo in <string|function> [not] to be <string> <...foo|object>');
+            expect.parseAssertion('<string|function> [not] to be <string> <foo*|object>');
+        }, 'to throw', 'Unknown type: foo in <string|function> [not] to be <string> <foo*|object>');
     });
 
     it('throw if the subject type is not specified', function () {
         expect(function () {
-            expect.parseAssertion('[not] to be <string> <...string|object>');
-        }, 'to throw', 'Missing subject type in [not] to be <string> <...string|object>');
+            expect.parseAssertion('[not] to be <string> <string*|object>');
+        }, 'to throw', 'Missing subject type in [not] to be <string> <string*|object>');
     });
 
     it('throw if the assertion cannot be detected', function () {
         expect(function () {
-            expect.parseAssertion('<string> <...string|object>');
-        }, 'to throw', 'Missing assertion in <string> <...string|object>');
+            expect.parseAssertion('<string> <string*|object>');
+        }, 'to throw', 'Missing assertion in <string> <string*|object>');
     });
 
     it('throw if varargs is used for the subject', function () {
         expect(function () {
-            expect.parseAssertion('<...any> [not] to be <...any>');
-        }, 'to throw', 'The subject type cannot have varargs: <...any> [not] to be <...any>');
+            expect.parseAssertion('<any*> [not] to be <any*>');
+        }, 'to throw', 'The subject type cannot have varargs: <any*> [not] to be <any*>');
     });
 
     it('throw if varargs is used before the last argument', function () {
         expect(function () {
-            expect.parseAssertion('<any> [not] to be <...any> <string>');
-        }, 'to throw', 'Only the last argument type can have varargs: <any> [not] to be <...any> <string>');
+            expect.parseAssertion('<any> [not] to be <any*> <string>');
+        }, 'to throw', 'Only the last argument type can have varargs: <any> [not] to be <any*> <string>');
     });
 });
