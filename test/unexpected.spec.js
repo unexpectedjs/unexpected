3872b1689114cee5db55aac3b26cf9c446889ae7
Sune Simonsen <sune@we-knowhow.dk>
2015-12-13 20:26:59 +0100

    Moved `when decoded as` spec to separate file.

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -8346,19 +8346,6 @@ describe('unexpected', function () {
         });
     });
 
-    describe('when decoded as assertion', function () {
-        it.skipIf(noBuffer, 'should decode a Buffer instance to utf-8', function () {
-            expect(new Buffer('æøå', 'utf-8'), 'when decoded as', 'utf-8', 'to equal', 'æøå');
-        });
-
-        it.skipIf(noBuffer,
-                  'should should provide the result as the fulfillment value if no assertion is provided', function () {
-            return expect(new Buffer('æøå', 'utf-8'), 'decoded as', 'utf-8').then(function (result) {
-                expect(result, 'to equal', 'æøå');
-            });
-        });
-    });
-
     describe('function type', function () {
         it('should inspect an empty function correctly', function () {
             expect(function () {}, 'to inspect as',
