94633fee856789f06acf1c1ca3480a5f9b9ba036
Sune Simonsen <sune@we-knowhow.dk>
2015-12-25 21:48:45 +0100

    Moved to error assertion tests to a separate file

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -120,50 +120,6 @@ describe('unexpected', function () {
         });
     });
 
-    describe('to error assertion', function () {
-        describe('with a function that throws', function () {
-            describe('with the "not" flag', function () {
-                it('should indicate that the function threw', function () {
-                    expect(function () {
-                        expect(function () {
-                            throw new Error('yikes');
-                        }, 'not to error');
-                    }, 'to throw',
-                        "expected function () { throw new Error('yikes'); } not to error\n" +
-                        "  threw: Error('yikes')"
-                    );
-                });
-            });
-        });
-
-        describe('with a function that returns a promise that is rejected', function () {
-            describe('with the "not" flag', function () {
-                it('should indicate that the function returned a rejected promise', function () {
-                    return expect(
-                        expect(function () {
-                            return expect.promise(function (resolve, reject) {
-                                setTimeout(function () {
-                                    reject(new Error('wat'));
-                                }, 1);
-                            });
-                        }, 'not to error'),
-                        'to be rejected with',
-                            "expected\n" +
-                            "function () {\n" +
-                            "  return expect.promise(function (resolve, reject) {\n" +
-                            "    setTimeout(function () {\n" +
-                            "      reject(new Error('wat'));\n" +
-                            "    }, 1);\n" +
-                            "  });\n" +
-                            "}\n" +
-                            "not to error\n" +
-                            "  returned promise rejected with: Error('wat')"
-                    );
-                });
-            });
-        });
-    });
-
     describe('object type', function () {
         describe('#diff', function () {
             it('should show identical multiline values correctly in diffs', function () {
