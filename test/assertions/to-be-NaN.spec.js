18de9083256cf1bf4325d533ad8846cca48ddee9
Sune Simonsen <sune@we-knowhow.dk>
2015-12-25 22:10:05 +0100

    Moved to be NaN assertion tests to a separate file

---
+++ test/assertions/to-be-NaN.spec.js
@@ -0,0 +1,17 @@
+/*global expect*/
+describe('to be NaN assertion', function () {
+    it('assert that the value is NaN or not', function () {
+        expect(NaN, 'to be NaN');
+        expect(2, 'not to be NaN');
+    });
+
+    it('fails when the assertion fails', function () {
+        expect(function () {
+            expect(0, 'to be NaN');
+        }, 'to throw', "expected 0 to be NaN");
+
+        expect(function () {
+            expect(NaN, 'not to be NaN');
+        }, 'to throw', "expected NaN not to be NaN");
+    });
+});
--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -776,23 +776,6 @@ describe('unexpected', function () {
         });
     });
 
-    describe('to be NaN assertion', function () {
-        it('assert that the value is NaN or not', function () {
-            expect(NaN, 'to be NaN');
-            expect(2, 'not to be NaN');
-        });
-
-        it('fails when the assertion fails', function () {
-            expect(function () {
-                expect(0, 'to be NaN');
-            }, 'to throw', "expected 0 to be NaN");
-
-            expect(function () {
-                expect(NaN, 'not to be NaN');
-            }, 'to throw', "expected NaN not to be NaN");
-        });
-    });
-
     describe('fail assertion', function () {
         it('throws an error', function () {
             expect(function () {
