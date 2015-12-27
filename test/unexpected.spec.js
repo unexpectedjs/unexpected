7ba9c5eb2436ca74f6c0f1c6d44d0311646321a7
Sune Simonsen <sune@we-knowhow.dk>
2015-12-27 21:17:02 +0100

    Moved UnexpectedError tests to a separate file

--- test/unexpected.spec.js
+++ test/unexpected.spec.js
@@ -27,45 +27,6 @@ describe('unexpected', function () {
         });
     });
 
-    describe('UnexpectedError', function () {
-        describe('with a single line message', function () {
-            it('should be inspected correctly', function () {
-                expect(function () {
-                    expect(2, 'to equal', 4);
-                }, 'to throw', function (err) {
-                    expect(err, 'to inspect as', 'UnexpectedError(expected 2 to equal 4)');
-                });
-            });
-        });
-
-        describe('with a multiline message', function () {
-            it('should be inspected correctly', function () {
-                expect(function () {
-                    expect('foo', 'to equal', 'bar');
-                }, 'to throw', function (err) {
-                    expect(
-                        err,
-                        'to inspect as',
-                        "UnexpectedError(\n" +
-                        "  expected 'foo' to equal 'bar'\n" +
-                        "\n" +
-                        "  -foo\n" +
-                        "  +bar\n" +
-                        ")"
-                    );
-                });
-            });
-        });
-
-        it('#getKeys should return a trimmed-down list', function () {
-            expect(function () {
-                expect('foo', 'to equal', 'bar');
-            }, 'to throw', function (err) {
-                expect(expect.findTypeOf(err).getKeys(err), 'to equal', [ 'message', 'errorMode', 'parent' ]);
-            });
-        });
-    });
-
     it('throws if the assertion does not exist', function () {
         expect(function () {
             expect({}, "to bee", 2);
