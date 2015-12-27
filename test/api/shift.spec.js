4a7f7510047238fcc745fe00e5a24e886bdc8f33
Sune Simonsen <sune@we-knowhow.dk>
2015-12-27 21:05:01 +0100

    Moved expect.shift tests to a separate file

---
+++ test/api/shift.spec.js
@@ -0,0 +1,141 @@
+/*global expect*/
+describe('expect.shift', function () {
+    describe('when preserving the subject by passing no arguments', function () {
+        it('should succeed', function () {
+            var clonedExpect = expect.clone().addAssertion('<string> blabla <assertion>', function (expect, subject) {
+                return expect.shift();
+            });
+            clonedExpect('foo', 'blabla', 'to equal', 'foo');
+        });
+
+        it('should fail with a diff', function () {
+            var clonedExpect = expect.clone().addAssertion('<string> blabla <assertion>', function (expect, subject) {
+                return expect.shift();
+            });
+            expect(function () {
+                clonedExpect('foo', 'blabla', 'to equal', 'foobar');
+            }, 'to throw',
+                   "expected 'foo' blabla to equal 'foobar'\n" +
+                   "\n" +
+                   "-foo\n" +
+                   "+foobar"
+                  );
+        });
+    });
+
+    it('should support calling shift multiple times', function () {
+        var clonedExpect = expect.clone().addAssertion('<number> up to [and including] <number> <assertion>', function (expect, subject, value) {
+            expect.errorMode = 'nested';
+            var numbers = [];
+            for (var i = subject ; i < (expect.flags['and including'] ? value + 1 : value) ; i += 1) {
+                numbers.push(i);
+            }
+            return expect.promise.all(numbers.map(function (number) {
+                return expect.promise(function () {
+                    return expect.shift(number);
+                });
+            }));
+        });
+
+        return expect(function () {
+            clonedExpect(5, 'up to and including', 100, 'to be within', 1, 90);
+        }, 'to error',
+                      'expected 5 up to and including 100 to be within 1, 90\n' +
+                      '  expected 91 to be within 1..90'
+                     );
+    });
+
+    describe('when substituting a different subject by passing a single argument', function () {
+        it('should succeed', function () {
+            var clonedExpect = expect.clone().addAssertion('<string> when appended with bar <assertion>', function (expect, subject) {
+                return expect.shift(subject + 'bar');
+            });
+            clonedExpect('foo', 'when appended with bar', 'to equal', 'foobar');
+        });
+
+        it('should fail with a diff', function () {
+            var clonedExpect = expect.clone().addAssertion('<string> when appended with bar <assertion>', function (expect, subject) {
+                return expect.shift(subject + 'bar');
+            });
+            expect(function () {
+                clonedExpect('crow', 'when appended with bar', 'to equal', 'foobar');
+            }, 'to throw',
+                   "expected 'crow' when appended with bar to equal 'foobar'\n" +
+                   "\n" +
+                   "-crowbar\n" +
+                   "+foobar"
+                  );
+        });
+    });
+
+    it('should identify the assertions even when the next assertion fails before shifting', function () {
+        var clonedExpect = expect.clone().addAssertion('<string> when appended with bar <assertion>', function (expect, subject) {
+            if (subject === 'crow') {
+                expect.fail();
+            }
+            return expect.shift(subject + 'bar');
+        });
+        expect(function () {
+            clonedExpect('crow', 'when appended with bar', 'when appended with bar', 'to equal', 'foobarbar');
+        }, 'to throw',
+               "expected 'crow' when appended with bar when appended with bar to equal 'foobarbar'"
+              );
+    });
+
+    it('supports the legacy 3 argument version', function () {
+        var clonedExpect = expect.clone().addAssertion('<string> when prepended with foo <assertion>', function (expect, subject) {
+            return this.shift(expect, 'foo' + subject, 0);
+        });
+        clonedExpect('foo', 'when prepended with foo', expect.it('to equal', 'foofoo'));
+    });
+
+    describe('with the legacy 2 argument version', function () {
+        it('inspects multiple arguments correctly', function () {
+            var clonedExpect = expect.clone().addAssertion('<string> when surrounded by <string> <string> <assertion>', function (expect, subject) {
+                return expect.shift('foo' + subject, 2);
+            });
+
+            return expect(function () {
+                clonedExpect('bar', 'when surrounded by', 'foo', 'quux', 'to be a number');
+            }, 'to throw',
+                          "expected 'bar' when surrounded by 'foo', 'quux' to be a number"
+                         );
+        });
+    });
+
+    describe('with an expect.it function as the next argument', function () {
+        it('should succeed', function () {
+            var clonedExpect = expect.clone().addAssertion('<string> when prepended with foo <assertion>', function (expect, subject) {
+                return expect.shift('foo' + subject);
+            });
+            clonedExpect('foo', 'when prepended with foo', expect.it('to equal', 'foofoo'));
+        });
+    });
+
+    it('should fail when the next argument is a non-expect.it function', function () {
+        var clonedExpect = expect.clone().addAssertion('<string> when prepended with foo <assertion>', function (expect, subject) {
+            return expect.shift('foo' + subject);
+        });
+        expect(function () {
+            clonedExpect('foo', 'when prepended with foo', function () {});
+        }, 'to throw',
+               "expected 'foo' when prepended with foo function () {}\n" +
+               "  No matching assertion, did you mean:\n" +
+               "  <string> when prepended with foo <assertion>"
+              );
+    });
+
+    describe('with an async assertion', function () {
+        it('should succeed', function () {
+            return expect(42, 'when delayed a little bit', 'to be a number');
+        });
+
+        it('should fail with a diff', function () {
+            return expect(
+                expect(false, 'when delayed a little bit', 'to be a number'),
+                'to be rejected with',
+                'expected false when delayed a little bit to be a number'
+            );
+        });
+    });
+});
