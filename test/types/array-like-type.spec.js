8e1e096ef41bf731d36feed222b1e248afd34c64
Andreas Lind <andreas@one.com>
2016-01-20 10:24:09 +0100

    Also support the new options in the array-like diff, added tests.

---
+++ test/types/array-like-type.spec.js
@@ -0,0 +1,113 @@
+/*global expect*/
+describe('array-like type', function () {
+    describe('with a subtype that disables indentation', function () {
+        var clonedExpect = expect.clone();
+
+        clonedExpect.addType({
+            base: 'array-like',
+            name: 'bogusarray',
+            identify: Array.isArray,
+            indent: false
+        });
+
+        it('should not render the indentation when an instance is inspected in a multi-line context', function () {
+            expect(
+                clonedExpect.inspect([
+                    'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                    'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+                ]).toString(),
+                'to equal',
+                "[\n" +
+                "'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',\n" +
+                "'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'\n" +
+                "]"
+            );
+        });
+
+        it('should not render the indentation when an instance is diffed', function () {
+            expect(
+                clonedExpect.diff(['a', 'b'], ['aa', 'bb']).diff.toString(),
+                'to equal',
+                "[\n" +
+                "'a', // should equal 'aa'\n" +
+                "     // -a\n" +
+                "     // +aa\n" +
+                "'b' // should equal 'bb'\n" +
+                "    // -b\n" +
+                "    // +bb\n" +
+                "]"
+            );
+        });
+
+        it('should not render the indentation when an instance participates in a "to satisfy" diff', function () {
+            expect(function () {
+                clonedExpect(['aaa', 'bbb'], 'to satisfy', { 0: 'foo' });
+            }, 'to throw',
+                "expected [ 'aaa', 'bbb' ] to satisfy { 0: 'foo' }\n" +
+                "\n" +
+                "[\n" +
+                "'aaa', // should equal 'foo'\n" +
+                "       // -aaa\n" +
+                "       // +foo\n" +
+                "'bbb'\n" +
+                "]"
+            );
+        });
+    });
+
+    describe('with a subtype that disables prefix, suffix, leading and trailing newline', function () {
+        var clonedExpect = expect.clone();
+
+        clonedExpect.addType({
+            base: 'array-like',
+            name: 'bogusarray',
+            identify: Array.isArray,
+            prefix: function (output) {
+                return output;
+            },
+            suffix: function (output) {
+                return output;
+            },
+            leadingNewline: false,
+            trailingNewline: false
+        });
+
+        it('should not render the newlines when an instance is inspected in a multi-line context', function () {
+            expect(
+                clonedExpect.inspect([
+                    'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                    'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+                ]).toString(),
+                'to equal',
+                "  'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',\n" +
+                "  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'"
+            );
+        });
+
+        it('should not render the newlines when an instance is diffed', function () {
+            expect(
+                clonedExpect.diff(['a', 'b'], ['aa', 'bb']).diff.toString(),
+                'to equal',
+                "  'a', // should equal 'aa'\n" +
+                "       // -a\n" +
+                "       // +aa\n" +
+                "  'b' // should equal 'bb'\n" +
+                "      // -b\n" +
+                "      // +bb"
+            );
+        });
+
+        it('should not render the indentation when an instance participates in a "to satisfy" diff', function () {
+            expect(function () {
+                clonedExpect(['aaa', 'bbb'], 'to satisfy', { 0: 'foo' });
+            }, 'to throw',
+                "expected  'aaa', 'bbb'  to satisfy { 0: 'foo' }\n" +
+                "\n" +
+                "  'aaa', // should equal 'foo'\n" +
+                "         // -aaa\n" +
+                "         // +foo\n" +
+                "  'bbb'"
+            );
+        });
+    });
+});
