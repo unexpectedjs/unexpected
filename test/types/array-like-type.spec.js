2eaf6cd55b77fd1853b6978f0e75b0ef49aa6129
Andreas Lind <andreas@one.com>
2016-02-17 08:49:23 +0100

    Raise coverage of array-like inspect (unrelated).

--- test/types/array-like-type.spec.js
+++ test/types/array-like-type.spec.js
@@ -130,4 +130,58 @@ describe('array-like type', function () {
             );
         });
     });
+
+    function toArguments() {
+        return arguments;
+    }
+
+    describe('when both types have numericalPropertiesOnly set', function () {
+        it('should only compare numerical properties for equality', function () {
+            var a = toArguments(1, 2);
+            var b = toArguments(1, 2);
+            b.foo = 123;
+            expect(a, 'to equal', b);
+        });
+
+        it('should fail when a numerical property has different values', function () {
+            var a = toArguments(1, 3);
+            var b = toArguments(1, 2);
+            expect(a, 'not to equal', b);
+        });
+    });
+
+    describe('with a custom subtype that comes with its own getKeys', function () {
+        it('should bar', function () {
+            var a = [ 'a' ];
+            var b = [ 'a' ];
+
+            var clonedExpect = expect.clone().addType({
+                name: 'foo',
+                base: 'array-like',
+                identify: Array.isArray,
+                numericalPropertiesOnly: false,
+                getKeys: function (obj) {
+                    var keys = this.baseType.getKeys(obj);
+                    if (obj === a) {
+                        keys.push('foobar');
+                    }
+                    return keys;
+                }
+            });
+            expect(function () {
+                clonedExpect(a, 'to equal', b);
+            }, 'to throw',
+                "expected [ 'a', foobar: undefined ] to equal [ 'a' ]\n" +
+                "\n" +
+                "[\n" +
+                "  'a'\n" +
+                "  // missing foobar: undefined\n" +
+                "]"
+            );
+        });
+    });
+
+    it('should inspect as [...] at depth 2+', function () {
+        expect([[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]]], 'to inspect as', '[ [ [...] ] ]');
+    });
 });
