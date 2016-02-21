07fb732436f0cec3299ad3c63cdfc69b9750889a
Sune Simonsen <sune@we-knowhow.dk>
2016-02-21 14:01:44 +0100

    Test the stack trace trimming

--- test/api/UnexpectedError.spec.js
+++ test/api/UnexpectedError.spec.js
@@ -36,4 +36,69 @@ describe('UnexpectedError', function () {
             expect(expect.findTypeOf(err).getKeys(err), 'to equal', [ 'message', 'errorMode', 'parent' ]);
         });
     });
+
+    describe('when full stack traces is disabled', function () {
+        it('trims the stack for node_module/unexpected/ and node_module/unexpected-<plugin-name>/', function () {
+            expect(function () {
+                expect.fail('wat');
+            }, 'to throw', function (err) {
+                err.useFullStackTrace = false;
+                err._hasSerializedErrorMessage = false;
+                err.stack =
+                    'UnexpectedError:\n' +
+                    'wat\n' +
+                    '      at oathbreaker (node_modules/unexpected/lib/oathbreaker.js:46:19)\n' +
+                    '      at Function.Unexpected.withError (node_modules/unexpected-sinon/lib/unexpected-sinon.js:123:1)\n' +
+                    '      at Function.Unexpected.withError (node_modules/unexpected/lib/Unexpected.js:792:12)\n' +
+                    '      at Function.<anonymous> (node_modules/unexpected/lib/assertions.js:569:16)\n' +
+                    '      at executeExpect (node_modules/unexpected/lib/Unexpected.js:1103:50)\n' +
+                    '      at Unexpected.expect (node_modules/unexpected/lib/Unexpected.js:1111:22)\n' +
+                    '      at Context.<anonymous> (test/Insection.spec.js:48:17)';
+
+                err.serializeMessage('text');
+
+                expect(err, 'to satisfy', {
+                    stack: 'UnexpectedError:\n' +
+                        'wat\n' +
+                        '      at Context.<anonymous> (test/Insection.spec.js:48:17)\n' +
+                        '      set UNEXPECTED_FULL_TRACE=true to see the full stack trace'
+                });
+            });
+        });
+    });
+
+    describe('when full stack traces is enabled', function () {
+        it('the initial stack is preserved', function () {
+            expect(function () {
+                expect.fail('wat');
+            }, 'to throw', function (err) {
+                err.useFullStackTrace = true;
+                err._hasSerializedErrorMessage = false;
+                err.stack =
+                    'UnexpectedError:\n' +
+                    'wat\n' +
+                    '      at oathbreaker (node_modules/unexpected/lib/oathbreaker.js:46:19)\n' +
+                    '      at Function.Unexpected.withError (node_modules/unexpected-sinon/lib/unexpected-sinon.js:123:1)\n' +
+                    '      at Function.Unexpected.withError (node_modules/unexpected/lib/Unexpected.js:792:12)\n' +
+                    '      at Function.<anonymous> (node_modules/unexpected/lib/assertions.js:569:16)\n' +
+                    '      at executeExpect (node_modules/unexpected/lib/Unexpected.js:1103:50)\n' +
+                    '      at Unexpected.expect (node_modules/unexpected/lib/Unexpected.js:1111:22)\n' +
+                    '      at Context.<anonymous> (test/Insection.spec.js:48:17)';
+
+                err.serializeMessage('text');
+
+                expect(err, 'to satisfy', {
+                    stack: 'UnexpectedError:\n' +
+                        'wat\n' +
+                        '      at oathbreaker (node_modules/unexpected/lib/oathbreaker.js:46:19)\n' +
+                        '      at Function.Unexpected.withError (node_modules/unexpected-sinon/lib/unexpected-sinon.js:123:1)\n' +
+                        '      at Function.Unexpected.withError (node_modules/unexpected/lib/Unexpected.js:792:12)\n' +
+                        '      at Function.<anonymous> (node_modules/unexpected/lib/assertions.js:569:16)\n' +
+                        '      at executeExpect (node_modules/unexpected/lib/Unexpected.js:1103:50)\n' +
+                        '      at Unexpected.expect (node_modules/unexpected/lib/Unexpected.js:1111:22)\n' +
+                        '      at Context.<anonymous> (test/Insection.spec.js:48:17)'
+                });
+            });
+        });
+    });
 });
