3e524aee46234e2c47760f238cad415f817442a3
Andreas Lind <andreas@one.com>
2016-03-07 00:32:23 +0100

    Suppress string diff when actual or expected is longer than 1024 chars.

---
+++ test/types/string-type.spec.js
@@ -0,0 +1,20 @@
+/*global expect*/
+describe('string type', function () {
+    it('should suppress a diff when the actual value has a length greater than 1 KB', function () {
+        var longString = new Array(1026).join('A');
+        expect(function () {
+            expect(longString, 'to equal', 'foo');
+        }, 'to throw',
+            "expected \'" + longString + "\'\nto equal \'foo\'\n\nDiff suppressed due to size > 1024"
+        );
+    });
+
+    it('should suppress a diff when the expected value has a length greater than 1 KB', function () {
+        var longString = new Array(1026).join('A');
+        expect(function () {
+            expect('foo', 'to equal', longString);
+        }, 'to throw',
+            "expected 'foo'\nto equal \'" + longString + "\'\n\nDiff suppressed due to size > 1024"
+        );
+    });
+});
