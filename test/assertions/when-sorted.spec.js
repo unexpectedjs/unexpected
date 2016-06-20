5f9ba01c899327b2f4385e88f1f92542d4ff297a
Joel Mukuthu <jmu@one.com>
2016-06-20 10:53:12 +0200

    Add 'when sorted' and 'when sorted by' assertions for arrays
    <array-like> when sorted <assertion?>
    <array-like> when sorted by <function> <assertion?>

---
+++ test/assertions/when-sorted.spec.js
@@ -0,0 +1,16 @@
+/*global expect*/
+describe('when sorted assertion', function () {
+    it('should should sort an array (in place) using the default compare function', function () {
+        expect(['c', 'a', 'b'], 'when sorted', 'to equal', ['a', 'b', 'c']);
+    });
+
+    it('should should provide the result as the fulfillment value if no assertion is provided', function () {
+        return expect(['c', 'b', 'a'], 'when sorted').then(function (sortedArray) {
+            expect(sortedArray, 'to equal', ['a', 'b', 'c']);
+        });
+    });
+
+    it('should also work without the \'when\'', function () {
+        expect(['c', 'd', 'a'], 'sorted', 'to equal', ['a', 'c', 'd']);
+    });
+});
