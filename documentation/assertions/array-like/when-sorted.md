e91871acd54dd5b5608f066dfd580348e8c188f0
Joel Mukuthu <jmu@one.com>
2016-06-22 10:25:04 +0200

    Add 'when sorted numerically'
    Add support for sorting numerical arrays to the 'when sorted'
    assertion.

--- documentation/assertions/array-like/when-sorted.md
+++ documentation/assertions/array-like/when-sorted.md
@@ -5,6 +5,12 @@ then delegate the return value to another assertion.
 expect(['c', 'a', 'b'], 'when sorted', 'to equal', ['a', 'b', 'c']);
 ```
 
+To sort an array of numbers, use `when sorted numerically`:
+
+```js
+expect([3, 1, 2], 'when sorted numerically', 'to equal', [1, 2, 3]);
+```
+
 In case of a failing expectation you get the following output:
 
 ```js
--- test/assertions/when-sorted.spec.js
+++ test/assertions/when-sorted.spec.js
@@ -4,6 +4,10 @@ describe('when sorted assertion', function () {
         expect(['c', 'a', 'b'], 'when sorted', 'to equal', ['a', 'b', 'c']);
     });
 
+    it('should sort an array of numbers if the \'numerically\' flag is included', function () {
+        expect([5, 12, 20, 2], 'when sorted numerically', 'to equal', [2, 5, 12, 20]);
+    });
+
     it('should provide the result as the fulfillment value if no assertion is provided', function () {
         return expect(['c', 'b', 'a'], 'when sorted').then(function (sortedArray) {
             expect(sortedArray, 'to equal', ['a', 'b', 'c']);
