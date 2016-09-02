2866d1929c2d1efc88f09ba4b23588e207304e9e
Andreas Lind <andreas@one.com>
2016-09-02 09:09:44 +0200

    Update magicpen to 5.10.0.
    Update the expected HTML output of one test to account for the changes in
    the HTML serializer.

--- test/api/outputFormat.spec.js
+++ test/api/outputFormat.spec.js
@@ -7,8 +7,8 @@ describe('outputFormat', function () {
                 clonedExpect(42, 'to equal', 24);
             }, 'to throw', {
                 htmlMessage:
-                '<div style="font-family: monospace; white-space: nowrap">\n' +
-                    '  <div><span style="color: red; font-weight: bold">expected</span>&nbsp;<span style="color: #0086b3">42</span>&nbsp;<span style="color: red; font-weight: bold">to&nbsp;equal</span>&nbsp;<span style="color: #0086b3">24</span></div>\n' +
+                    '<div style="font-family: monospace; white-space: nowrap">' +
+                    '<div><span style="color: red; font-weight: bold">expected</span>&nbsp;<span style="color: #0086b3">42</span>&nbsp;<span style="color: red; font-weight: bold">to&nbsp;equal</span>&nbsp;<span style="color: #0086b3">24</span></div>' +
                     '</div>'
             });
 
