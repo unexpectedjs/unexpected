eac213ad24f4d5f4997e31f5924a12e2806b2f34
Andreas Lind <andreas@one.com>
2016-09-12 21:42:29 +0200

    Use greedy-interval-packer to avoid reserving too much horizontal space for the arrows.

--- test/styles/arrow.spec.js
+++ test/styles/arrow.spec.js
@@ -2,7 +2,7 @@
 describe('arrow', function () {
     it('should render an arrow with the given dimensions', function () {
         expect(
-            expect.createOutput('text').arrow({ width: 4, height: 5, down: true }).toString(),
+            expect.createOutput('text').arrow({ width: 4, height: 5, direction: 'down' }).toString(),
             'to equal',
             "┌───\n" +
             "│\n" +
@@ -14,7 +14,7 @@ describe('arrow', function () {
 
     it('should support top and left coordinates', function () {
         expect(
-            expect.createOutput('text').arrow({ top: 2, left: 3, width: 4, height: 5, down: true }).toString(),
+            expect.createOutput('text').arrow({ top: 2, left: 3, width: 4, height: 5, direction: 'down' }).toString(),
             'to equal',
             "\n" +
             "\n" +
@@ -26,27 +26,15 @@ describe('arrow', function () {
         );
     });
 
-    it('should render an arrow that points upwards when up:true is given', function () {
+    it('should render an arrow that points upwards when direction:up is given', function () {
         expect(
-            expect.createOutput('text').arrow({ width: 4, height: 5, up: true }).toString(),
+            expect.createOutput('text').arrow({ width: 4, height: 5, direction: 'up' }).toString(),
             'to equal',
             "┌──>\n" +
             "│\n" +
             "│\n" +
             "│\n" +
             "└───"
-        );
-    });
-
-    it('should render an arrow that points both upwards and downwards when down:true and up:true are given', function () {
-        expect(
-            expect.createOutput('text').arrow({ width: 4, height: 5, up: true, down: true }).toString(),
-            'to equal',
-            "┌──>\n" +
-            "│\n" +
-            "│\n" +
-            "│\n" +
-            "└──>"
         );
     });
 });
