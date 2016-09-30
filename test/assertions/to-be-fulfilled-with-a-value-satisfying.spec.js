fd46762f1d8f709a1202cc9695cabc683e844fda
Joel Mukuthu <jmu@one.com>
2016-09-30 15:55:48 +0200

    Add '<function> to be fulfilled with a value [exhaustively] satisfying <any>'

--- test/assertions/to-be-fulfilled-with-a-value-satisfying.spec.js
+++ test/assertions/to-be-fulfilled-with-a-value-satisfying.spec.js
@@ -40,19 +40,6 @@ describe('to be fulfilled with a value satisfying assertion', function () {
         );
     });
 
-    describe('without the "exhaustively" flag', function () {
-        it('doesn\'t error if the expected value doesn\'t contain all the values in the subject', function () {
-            return expect(expect.promise.resolve({
-                foo: 'foo',
-                bar: 'bar',
-                quux: 'quux'
-            }), 'to be fulfilled with a value satisfying', {
-                foo: 'foo',
-                bar: 'bar'
-            });
-        });
-    });
-
     describe('with the "exhaustively" flag', function () {
         it('errors if the expected value doesn\'t contain all the values in the subject', function () {
             return expect(
@@ -90,6 +77,123 @@ describe('to be fulfilled with a value satisfying assertion', function () {
                     "    quux: 'quux' // should be removed\n" +
                     "  }"
             );
+        });
+    });
+
+    describe('without the "exhaustively" flag', function () {
+        it('doesn\'t error if the expected value doesn\'t contain all the values in the subject', function () {
+            return expect(expect.promise.resolve({
+                foo: 'foo',
+                bar: 'bar',
+                quux: 'quux'
+            }), 'to be fulfilled with a value satisfying', {
+                foo: 'foo',
+                bar: 'bar'
+            });
+        });
+    });
+
+    describe('when passed a function as the subject', function () {
+        it('should fail if the function returns a promise that is fulfilled with the wrong value', function () {
+            expect(
+                function () {
+                    return expect(function () {
+                        return expect.promise.resolve(123);
+                    }, 'to be fulfilled with a value satisfying', 456);
+                },
+                'to throw',
+                "expected\n" +
+                "function () {\n" +
+                "  return expect.promise.resolve(123);\n" +
+                "}\n" +
+                "to be fulfilled with a value satisfying 456\n" +
+                "  expected Promise (fulfilled) => 123 to be fulfilled with a value satisfying 456\n" +
+                "    expected 123 to equal 456"
+            );
+        });
+
+        describe('with the "exhaustively" flag', function () {
+            it('errors if the expected value doesn\'t contain all the values in the subject', function () {
+                return expect(
+                    function () {
+                        return expect(
+                            function () {
+                                return Promise.resolve({
+                                    foo: 'foo',
+                                    bar: 'bar',
+                                    quux: 'quux'
+                                });
+                            },
+                            'to be fulfilled with a value exhaustively satisfying',
+                            {
+                                foo: 'foo',
+                                bar: 'bar'
+                            }
+                        );
+                    },
+                    'to be rejected'
+                );
+            });
+
+            it('errors with the correct error', function () {
+                return expect(
+                    function () {
+                        return expect(
+                            function () {
+                                return Promise.resolve({
+                                    foo: 'foo',
+                                    bar: 'bar',
+                                    quux: 'quux'
+                                });
+                            },
+                            'to be fulfilled with a value exhaustively satisfying',
+                            {
+                                foo: 'foo',
+                                bar: 'bar'
+                            }
+                        );
+                    },
+                    'to be rejected with',
+                    "expected\n" +
+                    "function () {\n" +
+                    "  return Promise.resolve({\n" +
+                    "    foo: 'foo',\n" +
+                    "    bar: 'bar',\n" +
+                    "    quux: 'quux'\n" +
+                    "  });\n" +
+                    "}\n" +
+                    "to be fulfilled with a value exhaustively satisfying { foo: 'foo', bar: 'bar' }\n" +
+                    "  expected Promise (fulfilled) => { foo: 'foo', bar: 'bar', quux: 'quux' }\n" +
+                    "  to be fulfilled with a value exhaustively satisfying { foo: 'foo', bar: 'bar' }\n" +
+                    "    expected { foo: 'foo', bar: 'bar', quux: 'quux' }\n" +
+                    "    to exhaustively satisfy { foo: 'foo', bar: 'bar' }\n" +
+                    "\n" +
+                    "    {\n" +
+                    "      foo: 'foo',\n" +
+                    "      bar: 'bar',\n" +
+                    "      quux: 'quux' // should be removed\n" +
+                    "    }"
+                );
+            });
+        });
+
+        describe('without the "exhaustively" flag', function () {
+            it('doesn\'t error if the expected value doesn\'t contain all the values in the subject', function () {
+                return expect(
+                    function () {
+                        return expect.promise.resolve({
+                            foo: 'foo',
+                            bar: 'bar',
+                            quux: 'quux'
+                        });
+                    },
+                    'to be fulfilled with a value satisfying',
+                    {
+                        foo: 'foo',
+                        bar: 'bar'
+                    }
+                );
+            });
         });
     });
 });
