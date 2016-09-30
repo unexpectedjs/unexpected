adef1e66b1e6da2fe31ac1730785bd2fe6840e4b
Joel Mukuthu <jmu@one.com>
2016-09-30 11:53:39 +0200

    Separate tests for 'to be fulfilled' and 'to be fulfilled with' assertions

--- test/assertions/to-be-fulfilled.spec.js
+++ test/assertions/to-be-fulfilled.spec.js
@@ -4,76 +4,36 @@ describe('to be fulfilled assertion', function () {
         require('rsvp').Promise :
         window.RSVP.Promise;
 
-    describe('with no additional argument', function () {
-        it('should succeed if the response is resolved with any value', function () {
-            return expect(new Promise(function (resolve, reject) {
-                setTimeout(function () {
-                    resolve('yay');
-                }, 0);
-            }), 'to be fulfilled');
-        });
-
-        it('should fail if the promise is rejected', function () {
-            return expect(
-                expect(new Promise(function (resolve, reject) {
-                    setTimeout(function () {
-                        reject('unhappy times');
-                    }, 0);
-                }), 'to be fulfilled'),
-                'to be rejected with',
-                "expected Promise to be fulfilled\n" +
-                    "  Promise unexpectedly rejected with 'unhappy times'"
-            );
-        });
-
-        it('should fail with the correct message if the promise is rejected without a reason', function () {
-            return expect(
-                expect(new Promise(function (resolve, reject) {
-                    setTimeout(reject, 0);
-                }), 'to be fulfilled'),
-                'to be rejected with',
-                "expected Promise to be fulfilled\n" +
-                    "  Promise unexpectedly rejected"
-            );
-        });
+    it('should succeed if the response is resolved with any value', function () {
+        return expect(new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve('yay');
+            }, 0);
+        }), 'to be fulfilled');
     });
 
-    describe('with an additional argument', function () {
-        it('should succeed if the response is resolved with a reason satisfying the argument', function () {
-            return expect(new Promise(function (resolve, reject) {
+    it('should fail if the promise is rejected', function () {
+        return expect(
+            expect(new Promise(function (resolve, reject) {
                 setTimeout(function () {
-                    resolve(123);
+                    reject('unhappy times');
                 }, 0);
-            }), 'to be fulfilled with', 123);
-        });
+            }), 'to be fulfilled'),
+            'to be rejected with',
+            "expected Promise to be fulfilled\n" +
+                "  Promise unexpectedly rejected with 'unhappy times'"
+        );
+    });
 
-        it('should forward the fulfillment value', function () {
-            return expect(expect.promise.resolve(123), 'to be fulfilled with', 123).then(function (value) {
-                expect(value, 'to equal', 123);
-            });
-        });
-
-        it('should fail if the promise is resolved with a value that does not satisfy the argument', function () {
-            return expect(
-                expect(new Promise(function (resolve, reject) {
-                    setTimeout(function () {
-                        resolve({ foo: 'bar', baz: 'quux' });
-                    }, 1);
-                }), 'to be fulfilled with', { baz: 'qux' }),
-                'to be rejected with',
-                "expected Promise to be fulfilled with { baz: 'qux' }\n" +
-                    "  expected { foo: 'bar', baz: 'quux' } to satisfy { baz: 'qux' }\n" +
-                    "\n" +
-                    "  {\n" +
-                    "    foo: 'bar',\n" +
-                    "    baz: 'quux' // should equal 'qux'\n" +
-                    "                //\n" +
-                    "                // -quux\n" +
-                    "                // +qux\n" +
-                    "  }"
-
-            );
-        });
+    it('should fail with the correct message if the promise is rejected without a reason', function () {
+        return expect(
+            expect(new Promise(function (resolve, reject) {
+                setTimeout(reject, 0);
+            }), 'to be fulfilled'),
+            'to be rejected with',
+            "expected Promise to be fulfilled\n" +
+                "  Promise unexpectedly rejected"
+        );
     });
 
     describe('when passed a function', function () {
@@ -108,22 +68,6 @@ describe('to be fulfilled assertion', function () {
                 "to be fulfilled\n" +
                 "  expected Promise (rejected) => Error('foo') to be fulfilled\n" +
                 "    Promise (rejected) => Error('foo') unexpectedly rejected with Error('foo')"
-            );
-        });
-
-        it('should fail if the function returns a promise that is fulfilled with the wrong value', function () {
-            expect(function () {
-                return expect(function () {
-                    return expect.promise.resolve(123);
-                }, 'to be fulfilled with', 456);
-            }, 'to throw',
-                "expected\n" +
-                "function () {\n" +
-                "  return expect.promise.resolve(123);\n" +
-                "}\n" +
-                "to be fulfilled with 456\n" +
-                "  expected Promise (fulfilled) => 123 to be fulfilled with 456\n" +
-                "    expected 123 to equal 456"
             );
         });
 
