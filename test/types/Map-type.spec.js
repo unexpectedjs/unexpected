6502e62924a151ed3e8268cf8dbce8b6d775b627
Alex J Burke <alex@alexjeffburke.com>
2017-08-20 13:52:01 +0200

    Adjust "to have values satisfying" to work with Map.

--- test/types/Map-type.spec.js
+++ test/types/Map-type.spec.js
@@ -99,4 +99,25 @@
             "])"
         );
     });
+
+    it('should have values satisfying an object', function () {
+        expect(function () {
+            expect(new Map([[[], { foo: null }]]), 'to have values satisfying', { foo: expect.it('to be null') });
+        }, 'not to error');
+    });
+
+    it('should output a diff when failing "to have values satisfying"', function () {
+        expect(function () {
+            expect(new Map([[[], { foo: null }]]), 'to have values satisfying', { foo: expect.it('to be a number') });
+        }, 'to error',
+            'expected Map([ [[], { foo: null }] ])\n' +
+            "to have values satisfying { foo: expect.it('to be a number') }\n" +
+            '\n' +
+            'Map([\n' +
+            '  [[], {\n' +
+            '    foo: null // should be a number\n' +
+            '  }]\n' +
+            '])'
+        );
+    });
 });
