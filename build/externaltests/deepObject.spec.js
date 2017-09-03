934a29a91932870a015bb21384941ade761aee15
Sune Simonsen <sune@we-knowhow.dk>
2017-09-03 11:39:38 +0200

    Updated site

--- build/externaltests/deepObject.spec.js
+++
@@ -1,6 +0,0 @@
-var expect = require('../lib');
-it('should not dot out the object in the diff', function () {
-    expect(function () {
-        expect({ 0: { 0: { 0: { 0: { 0: { 0: {} } } } } } }, 'to equal', {});
-    }, 'to throw', "expected { 0: { 0: { 0: { 0: { 0: { 0: ... } } } } } } to equal {}\n" + "\n" + "{\n" + "  0: { 0: { 0: { 0: { 0: { 0: {} } } } } } // should be removed\n" + "}");
-});
--- build/test/styles/annotationBlock.spec.js
+++
@@ -1,6 +0,0 @@
-/*global expect*/
-describe('annotationBlock', function () {
-    it('should render an empty block', function () {
-        expect(expect.createOutput('text').annotationBlock(function () {}).toString(), 'to equal', '// ');
-    });
-});
