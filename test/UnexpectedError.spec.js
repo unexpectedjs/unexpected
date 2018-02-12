a35011602b7a03ebc7adb15dd5355ca20c01a09d
Andreas Lind <andreaslindpetersen@gmail.com>
2018-02-12 21:43:45 +0100

    UnexpectedError#stack: Don't mess up when the error message contains $&

---
+++ test/UnexpectedError.spec.js
@@ -0,0 +1,11 @@
+describe('UnexpectedError', function() {
+  describe('#stack', function() {
+    it('should not mess up when the error message contains $&', function() {
+      return expect(() => expect('$&', 'to equal', 'foo'), 'to error').then(
+        err => {
+          expect(err.stack, 'to contain', '$&');
+        }
+      );
+    });
+  });
+});
