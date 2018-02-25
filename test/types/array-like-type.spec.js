85736e69fab9e2883bf04ff315ec1621f9034fa7
Alex J Burke <alex@alexjeffburke.com>
2018-02-26 00:34:51 +0100

    Convert array-like inspect and diff to respect valueForKey().

--- test/types/array-like-type.spec.js
+++ test/types/array-like-type.spec.js
@@ -374,6 +374,38 @@ describe('array-like type', function() {
     });
   });
 
+  describe('with a custom subtype that comes with its own valueForKeys', function() {
+    it('should process the elements in both inspection and diff in "to equal"', function() {
+      var clonedExpect = expect.clone().addType({
+        name: 'firstElemUpper',
+        base: 'array-like',
+        identify: Array.isArray,
+        valueForKey: function(arr, key) {
+          var value = arr[key];
+          if (key === 0) {
+            return value.toUpperCase();
+          }
+          return value;
+        }
+      });
+      expect(
+        function() {
+          clonedExpect(['foobar', 'barbar'], 'to equal', ['foobar', 'barbaz']);
+        },
+        'to throw',
+        "expected [ 'FOOBAR', 'barbar' ] to equal [ 'FOOBAR', 'barbaz' ]\n" +
+          '\n' +
+          '[\n' +
+          "  'FOOBAR',\n" +
+          "  'barbar' // should equal 'barbaz'\n" +
+          '           //\n' +
+          '           // -barbar\n' +
+          '           // +barbaz\n' +
+          ']'
+      );
+    });
+  });
+
   it('should inspect as [...] at depth 2+', function() {
     expect(
       [[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]]],
