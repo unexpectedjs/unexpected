57da7c49e0cca897bf73d6116c81f2520383bb1d
Andreas Lind <andreaslindpetersen@gmail.com>
2018-03-29 16:07:58 +0200

    Only compact the subject when there is a diff

--- test/assertions/to-have-an-item-satisfying.spec.js
+++ test/assertions/to-have-an-item-satisfying.spec.js
@@ -114,7 +114,11 @@ describe('to have an item satisfying assertion', function() {
         );
       },
       'to throw',
-      'expected array to have an item satisfying\n' +
+      'expected\n' +
+        '[\n' +
+        '  [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]\n' +
+        ']\n' +
+        'to have an item satisfying\n' +
         'function (item) {\n' +
         '  expect.fail(function (output) {\n' +
         "    output.text('foo').nl().text('bar');\n" +
