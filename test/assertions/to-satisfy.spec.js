930431ca55d7260d8ea450a0a95d6c271bb05d75
Andreas Lind <andreas.lind@peakon.com>
2018-04-17 10:40:59 +0200

    Make expect({foo: <function>}, 'to satisfy', {foo: <function>}) compare the functions with === instead of executing the RHS one with the LHS one as the argument.
    (Breaking!)

--- test/assertions/to-satisfy.spec.js
+++ test/assertions/to-satisfy.spec.js
@@ -2232,23 +2232,26 @@ describe('to satisfy assertion', () => {
     });
   });
 
-  // Debatable:
-  describe('when an unpresent value to is satisfied against a function', () => {
-    it('should allow an unpresent value to be satisfied against a non-expect.it function', () => {
-      expect({}, 'to satisfy', { foo: function() {} });
+  describe('when an unpresent value to is satisfied against an expect.it function wrapper', () => {
+    it('should allow an unpresent value to be satisfied against the function', () => {
+      expect({}, 'to satisfy', {
+        foo: expect.it(v => {
+          expect(v, 'to be undefined');
+        })
+      });
     });
 
     it('should fail when the function throws', () => {
       expect(
-        function() {
+        () => {
           expect({}, 'to satisfy', {
-            foo(value) {
+            foo: expect.it(value => {
               expect(value, 'to be a string');
-            }
+            })
           });
         },
         'to throw',
-        function(err) {
+        err => {
           // Compensate for V8 5.1+ setting { foo: function () {} }.foo.name === 'foo'
           // http://v8project.blogspot.dk/2016/04/v8-release-51.html
           expect(
@@ -2271,7 +2274,7 @@ describe('to satisfy assertion', () => {
 
     it('should fail with a diff', () => {
       expect(
-        function() {
+        () => {
           expect({}, 'to satisfy', { foo: expect.it('to be a string') });
         },
         'to throw',
@@ -2287,7 +2290,12 @@ describe('to satisfy assertion', () => {
   it('should not break when the assertion fails and there is a fulfilled function in the RHS', () => {
     expect(
       function() {
-        expect({}, 'to satisfy', { bar: 123, foo: function() {} });
+        expect({}, 'to satisfy', {
+          bar: 123,
+          foo: expect.it(v => {
+            expect(v, 'to be undefined');
+          })
+        });
       },
       'to throw',
       function(err) {
@@ -2299,14 +2307,34 @@ describe('to satisfy assertion', () => {
             .toString()
             .replace(/function foo/g, 'function '),
           'to satisfy',
-          'expected {} to satisfy { bar: 123, foo: function () {} }\n' +
+          'expected {}\n' +
+            "to satisfy { bar: 123, foo: expect.it(v => { expect(v, 'to be undefined'); }) }\n" +
             '\n' +
             '{\n' +
             '  // missing bar: 123\n' +
-            '  // missing foo: should satisfy function () {}\n' +
+            "  // missing foo: should satisfy expect.it(v => { expect(v, 'to be undefined'); })\n" +
             '}'
         );
       }
+    );
+  });
+
+  it('should render a diff when the function differs', () => {
+    function myFunction() {}
+    function myOtherFunction() {}
+
+    expect(
+      () => {
+        expect({ foo: myFunction }, 'to satisfy', { foo: myOtherFunction });
+      },
+      'to throw',
+      'expected { foo: function myFunction() {} }\n' +
+        'to satisfy { foo: function myOtherFunction() {} }\n' +
+        '\n' +
+        '{\n' +
+        '  foo: function myFunction() {} // expected { foo: function myFunction() {} }\n' +
+        '                                // to satisfy { foo: function myOtherFunction() {} }\n' +
+        '}'
     );
   });
 
