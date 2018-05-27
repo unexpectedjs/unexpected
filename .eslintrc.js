10f8810b9636af94a27b4de4047eb61915ed89bc
Andreas Lind <andreaslindpetersen@gmail.com>
2018-05-27 11:40:16 +0200

    Run .eslintrc.js through prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
-const config ={
-    extends: [
-        'pretty-standard'
-    ],
-    plugins: ['import'],
-    rules: {
-        'import/no-extraneous-dependencies': [
-            'error', {
-                devDependencies: [ '**/test/**/*.js', '**/bootstrap-unexpected-markdown.js' ],
-                optionalDependencies: false,
-                peerDependencies: false
-            }
-        ]
-    }
+const config = {
+  extends: ['pretty-standard'],
+  plugins: ['import'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/test/**/*.js',
+          '**/bootstrap-unexpected-markdown.js'
+        ],
+        optionalDependencies: false,
+        peerDependencies: false
+      }
+    ]
+  }
 };
 
 if (process.stdin.isTTY) {
-    // Enable plugin-prettier when running in a terminal. Allows us to have
-    // eslint verify prettier formatting, while not being bothered by it in our
-    // editors.
-    config.plugins.push('prettier');
-    config.rules['prettier/prettier'] = 'error';
+  // Enable plugin-prettier when running in a terminal. Allows us to have
+  // eslint verify prettier formatting, while not being bothered by it in our
+  // editors.
+  config.plugins.push('prettier');
+  config.rules['prettier/prettier'] = 'error';
 }
 
 module.exports = config;
