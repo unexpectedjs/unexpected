e47a44a74b3bae269f0d1718a33572a068c885e1
Sune Simonsen <sune@we-knowhow.dk>
2018-12-11 22:26:15 +0100

    Added karma, mocha, chrome headless setup

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha'],
+
+    exclude: ['build/test/external.spec.js'],
+
+    files: [
+      'vendor/es5-shim.js',
+      'vendor/es5-sham.js',
+      'vendor/rsvp.js',
+      'vendor/unexpected-magicpen.min.js',
+      'build/test/promisePolyfill.js',
+      'unexpected.js',
+      'build/test/common.js',
+      'build/test/**/*.spec.js'
+    ],
+
+    browsers: ['ChromeHeadlessNoSandbox'],
+
+    client: {
+      mocha: {
+        reporter: 'html'
+      }
+    },
+
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
+  });
+};
