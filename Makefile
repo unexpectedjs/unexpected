unexpectedjs/unexpected

commit 6f5d4b298480faf2d70bf819d6bea0d4f5e7efba

Avoid rebuilding twice by not listing the sourcemap as a target.
The sourcemap is automatically output by the build/lib target.
This meant listing the source map then triggers rebuilding the
library a second time. Fix that.

Makefile
@@ -1,6 +1,6 @@
 REPORTER = dot
 
-TARGETS ?= unexpected.js unexpected.js.map
+TARGETS ?= unexpected.js
 
 CHEWBACCA_THRESHOLD ?= 25
 

commit 1f9956bd4045dd67c20df938051a92854be85091

Makefile: Reintroduce $TARGETS, still avoiding the double build
6f5d4b29 caused this error when trying to release a new version:

  npm ERR! Git working directory not clean.
  npm ERR! M unexpected.js.map

... because the commit-unexpected target no longer staged the
source map.

Seemed easiest to reintroduce the $TARGETS var.

Makefile
@@ -1,6 +1,8 @@
 REPORTER = dot
 
-TARGETS ?= unexpected.js
+TARGETS ?= unexpected.js unexpected.js.map
+.PHONY: unexpected.js
+.SECONDARY: unexpected.js.map
 
 CHEWBACCA_THRESHOLD ?= 25
 
@@ -25,7 +27,6 @@ build/externaltests: externaltests/*
 
 build: build/lib build/test build/externaltests
 
-.PHONY: ${TARGETS}
 ${TARGETS}: build
 	./node_modules/.bin/rollup --config rollup.config.js --sourcemap --format umd --name weknowhow.expect -o unexpected.js build/lib/index.js
 