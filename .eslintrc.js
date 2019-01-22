6fb7e0b8b12240d905779e326e1730b8bfa58a74
Andreas Lind <andreas.lind@peakon.com>
2019-01-22 10:43:55 +0100

    Use prettier --check instead of eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-const config = {
+module.exports = {
   extends: ['standard', 'prettier', 'prettier/standard'],
   plugins: ['import', 'mocha'],
   env: {
@@ -21,13 +21,3 @@ const config = {
     'mocha/no-identical-title': 'error'
   }
 };
-
-if (process.stdin.isTTY) {
-  // Enable plugin-prettier when running in a terminal. Allows us to have
-  // eslint verify prettier formatting, while not being bothered by it in our
-  // editors.
-  config.plugins.push('prettier');
-  config.rules['prettier/prettier'] = 'error';
-}
-
-module.exports = config;
