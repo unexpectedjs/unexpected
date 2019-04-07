57bb39dbc8cc1eec67cd72cdec94663afbf0355e
Andreas Lind <andreaslindpetersen@gmail.com>
2019-04-07 14:18:57 +0200

    Try the same trick with detect-indent

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,10 @@ module.exports = {
   plugins: [
     /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
     require('rollup-plugin-buble')({
-      include: ['node_modules/leven/**/*.js']
+      include: [
+        'node_modules/leven/**/*.js',
+        'node_modules/detect-indent/**/*.js'
+      ]
     }),
     require('rollup-plugin-commonjs')(),
     require('rollup-plugin-node-resolve')(),
