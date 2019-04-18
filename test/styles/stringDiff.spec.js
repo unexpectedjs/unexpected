2677d2b17254b23740d7d039e3e7cccfa06c7b98
Alex J Burke <alex@alexjeffburke.com>
2019-04-18 15:38:34 +0200

    Rework the stringDiff style to operate via unifiedDiff.
    Adapt the style code to handle changes passed up to it from the
    unifiedDiff module. Take care to retain the trailing newline handling
    and ensure carriage returns and correctly highlighted and returned.

--- test/styles/stringDiff.spec.js
+++ test/styles/stringDiff.spec.js
@@ -15,6 +15,7 @@ describe('stringDiff', () => {
           '-def\n' +
           ' ghi\n' +
           ' jkl\n' +
+          '...\n' +
           '-mno\n' +
           '+mno\n' +
           '+pqr\n' +
@@ -27,46 +28,34 @@ describe('stringDiff', () => {
         'to equal',
         expect
           .createOutput('text')
-          .block(function() {
-            this.diffRemovedLine('-')
-              .nl()
-              .diffRemovedLine('-');
-          })
-          .block(function() {
-            this.diffRemovedLine('abc')
-              .nl()
-              .diffRemovedLine('def');
-          })
+          .raw('-')
+          .diffRemovedLine('abc')
           .nl()
-          .text(' ghi')
+          .raw('-')
+          .diffRemovedLine('def')
           .nl()
-          .text(' jkl')
+          .raw(' ')
+          .text('ghi')
           .nl()
-          .block(function() {
-            this.diffRemovedLine('-');
-          })
-          .block(function() {
-            this.diffRemovedLine('mno');
-          })
+          .raw(' ')
+          .text('jkl')
           .nl()
-          .block(function() {
-            this.diffAddedLine('+')
-              .nl()
-              .diffAddedLine('+')
-              .nl()
-              .diffAddedLine('+')
-              .nl()
-              .diffAddedLine('+');
-          })
-          .block(function() {
-            this.diffAddedLine('mno')
-              .nl()
-              .diffAddedHighlight('pqr')
-              .nl()
-              .diffAddedHighlight('stu')
-              .nl()
-              .diffAddedHighlight('vwx');
-          })
+          .jsComment('...')
+          .nl()
+          .raw('-')
+          .diffRemovedLine('mno')
+          .nl()
+          .raw('+')
+          .diffAddedLine('mno')
+          .nl()
+          .raw('+')
+          .diffAddedHighlight('pqr')
+          .nl()
+          .raw('+')
+          .diffAddedHighlight('stu')
+          .nl()
+          .raw('+')
+          .diffAddedHighlight('vwx')
       );
     });
   });
@@ -85,6 +74,8 @@ describe('stringDiff', () => {
           .text('ghi')
           .nl()
           .text('jkl')
+          .nl()
+          .jsComment('...')
           .nl()
           .diffRemovedLine('mno')
           .nl()
