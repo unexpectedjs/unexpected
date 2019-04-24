48689df5a2a7a2b4f47569814dd2aa4030e1809f
Sune Simonsen <sune@we-knowhow.dk>
2019-04-24 20:50:36 +0200

    Make @papandreou's OCD happy

--- test/types/string-type.spec.js
+++ test/types/string-type.spec.js
@@ -1,24 +1,24 @@
 /* global expect */
 describe('string type', () => {
-  it('should suppress a diff when the actual value has a length greater than 40 KB', () => {
-    var longString = new Array(40980).join('A');
+  it('should suppress a diff when the actual value has a length greater than 64 KB', () => {
+    var longString = new Array(65538).join('A');
     expect(
       function() {
         expect(longString, 'to equal', 'foo');
       },
       'to throw',
-      `expected '${longString}'\nto equal 'foo'\n\nDiff suppressed due to size > 40960`
+      `expected '${longString}'\nto equal 'foo'\n\nDiff suppressed due to size > 65536`
     );
   });
 
-  it('should suppress a diff when the expected value has a length greater than 40 KB', () => {
-    var longString = new Array(40980).join('A');
+  it('should suppress a diff when the expected value has a length greater than 64 KB', () => {
+    var longString = new Array(65538).join('A');
     expect(
       function() {
         expect('foo', 'to equal', longString);
       },
       'to throw',
-      `expected 'foo'\nto equal '${longString}'\n\nDiff suppressed due to size > 40960`
+      `expected 'foo'\nto equal '${longString}'\n\nDiff suppressed due to size > 65536`
     );
   });
 });
