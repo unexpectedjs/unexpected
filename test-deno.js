0e5e0ae42f63ae5747d8858d5e9d5fc1551dea8c
Alex J Burke <alex@alexjeffburke.com>
2019-08-10 12:25:48 +0200

    Ensure an unclean exit with failures on deno.

--- test-deno.js
+++ test-deno.js
@@ -1,6 +1,8 @@
-/* eslint-disable import/first */
+/* globals Deno */
 import './deno-setup.js';
 import './test/common.js';
 import './build/tests.esm.js';
 
-window.mocha.run();
+window.mocha.run(failureCount => {
+  Deno.exit(failureCount > 0 ? 1 : 0);
+});
