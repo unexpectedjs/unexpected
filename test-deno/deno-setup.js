f5baae8aafed870166e5b14950c39e505e57933b
Alex J Burke <alex@alexjeffburke.com>
2019-08-25 12:37:58 +0200

    Collect deno test bits in a subfolder.

---
+++ test-deno/deno-setup.js
@@ -0,0 +1,11 @@
+import '../node_modules/mocha/mocha.js';
+import unexpected from '../unexpected-deno.js';
+import unexpectedMagicPen from '../node_modules/unexpected-magicpen/unexpected-magicpen.esm.js';
+
+window.mocha.reporter('spec');
+window.mocha.setup('bdd');
+
+window.weknowhow = {
+  expect: unexpected,
+  unexpectedMagicPen: unexpectedMagicPen
+};
--- test-deno/deno-test.js
+++ test-deno/deno-test.js
@@ -1,7 +1,7 @@
 /* globals Deno */
 import './deno-setup.js';
-import './test/common.js';
-import './build/tests.esm.js';
+import '../test/common.js';
+import '../build/tests.esm.js';
 
 window.mocha.run(failureCount => {
   Deno.exit(failureCount > 0 ? 1 : 0);
