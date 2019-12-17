bb03b566e2c5e0bc7bed6012a47ac425808269cd
Andreas Lind <andreaslindpetersen@gmail.com>
2019-12-17 23:47:31 +0100

    <BigInt> [not] to be greater than or equal to <BigInt>

--- test/assertions/to-be-greater-than-or-equal.spec.js
+++ test/assertions/to-be-greater-than-or-equal.spec.js
@@ -1,4 +1,4 @@
-/* global expect */
+/* global expect, BigInt */
 describe('greater than or equal assertion', () => {
   it('asserts >=', () => {
     expect(3, 'to be greater than or equal to', 2);
@@ -16,4 +16,39 @@ describe('greater than or equal assertion', () => {
       'expected -1 to be greater than or equal to 0'
     );
   });
+
+  if (typeof BigInt === 'function') {
+    describe('with BigInt', function() {
+      it('asserts >=', () => {
+        expect(BigInt(3), 'to be greater than or equal to', BigInt(2));
+        expect(BigInt(2), 'to be greater than or equal to', BigInt(2));
+      });
+
+      it('throws when the assertion fails', () => {
+        expect(
+          function() {
+            expect(BigInt(-1), 'to be greater than or equal to', BigInt(0));
+          },
+          'to throw exception',
+          'expected BigInt(-1) to be greater than or equal to BigInt(0)'
+        );
+      });
+
+      it('refuses to compare a BigInt to a number', () => {
+        expect(
+          function() {
+            expect(BigInt(-1), 'to be greater than or equal to', 0);
+          },
+          'to throw',
+          'expected BigInt(-1) to be greater than or equal to 0\n' +
+            '  The assertion does not have a matching signature for:\n' +
+            '    <BigInt> to be greater than or equal to <number>\n' +
+            '  did you mean:\n' +
+            '    <BigInt> [not] to be greater than or equal to <BigInt>\n' +
+            '    <number> [not] to be greater than or equal to <number>\n' +
+            '    <string> [not] to be greater than or equal to <string>'
+        );
+      });
+    });
+  }
 });
