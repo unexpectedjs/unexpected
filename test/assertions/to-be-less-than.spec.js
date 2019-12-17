ac3272004bbd8037edbca3589296a61a26ecb459
Andreas Lind <andreaslindpetersen@gmail.com>
2019-12-17 23:38:50 +0100

    <BigInt> [not] to be (less than|below) <BigInt>

--- test/assertions/to-be-less-than.spec.js
+++ test/assertions/to-be-less-than.spec.js
@@ -1,4 +1,4 @@
-/* global expect */
+/* global expect, BigInt */
 describe('less than assertion', () => {
   it('asserts <', () => {
     expect(0, 'to be less than', 4);
@@ -15,4 +15,39 @@ describe('less than assertion', () => {
       'expected 0 to be less than 0'
     );
   });
+
+  if (typeof BigInt === 'function') {
+    describe('with BigInt', function() {
+      it('asserts <', () => {
+        expect(BigInt(0), 'to be less than', BigInt(4));
+        expect(BigInt(0), 'to be below', BigInt(1));
+      });
+
+      it('throws when the assertion fails', () => {
+        expect(
+          function() {
+            expect(BigInt(0), 'to be less than', BigInt(0));
+          },
+          'to throw exception',
+          'expected BigInt(0) to be less than BigInt(0)'
+        );
+      });
+
+      it('refuses to compare to a number', () => {
+        expect(
+          function() {
+            expect(BigInt(0), 'to be less than', 1);
+          },
+          'to throw exception',
+          'expected BigInt(0) to be less than 1\n' +
+            '  The assertion does not have a matching signature for:\n' +
+            '    <BigInt> to be less than <number>\n' +
+            '  did you mean:\n' +
+            '    <BigInt> [not] to be (less than|below) <BigInt>\n' +
+            '    <number> [not] to be (less than|below) <number>\n' +
+            '    <string> [not] to be (less than|below) <string>'
+        );
+      });
+    });
+  }
 });
